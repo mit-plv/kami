1ab025d09eb5a8d6a86b3a19e52083b1ed8e7c71
Ben Sherman <sherm@mit.edu>
2015-12-21 21:01:03 -0500

    Actually handle ruleMeth part of labels properly in new semantics

--- src/SemanticsNew.v
+++ src/SemanticsNew.v
@@ -3,25 +3,50 @@ Require Import Syntax Semantics Refinement.
 
 Require Import String List.
 
-Definition bothNotRule (rm1 rm2 : bool) : Prop :=
-  rm1 = false \/ rm2 = false.
+Record LabelTP {A : Type} :=
+  { ruleMeth : option A
+  ; dms : CallsT
+  ; cms : CallsT
+  }.
 
-Inductive CanCombine : (RegsT * LabelT) -> (RegsT * LabelT) -> Prop :=
-  MkCanCombine : forall u1 rm1 ds1 cs1 u2 rm2 ds2 cs2,
-    MF.Disj u1 u2 -> MF.Disj ds1 ds2 -> MF.Disj cs1 cs2 -> bothNotRule rm1 rm2
-   -> CanCombine (u1, (rm1, ds1, cs1)) (u2, (rm2, ds2, cs2)).
+Arguments LabelTP : clear implicits.
 
-Definition mergeLabel (l1 l2 : LabelT) : LabelT := match l1, l2 with
-  (rm1, ds1, cs1), (rm2, ds2, cs2) =>
-    (orb rm1 rm2, MF.union ds1 ds2, MF.union cs1 cs2)
+Definition LabelT := LabelTP True.
+Definition RuleLabelT := LabelTP string.
+
+Definition mapLabelTP {A B : Type} (f : A -> B) (l : LabelTP A) 
+  : LabelTP B := match l with
+  | Build_LabelTP rm dms cms => Build_LabelTP _ (option_map f rm) dms cms
   end.
 
-Definition equivalent (l1 l2 : LabelT) (p : LabelT -> LabelT) :=
+Definition notBothRule (rm1 rm2 : option string) : Prop :=
+  rm1 = None \/ rm2 = None.
+
+Inductive CanCombine : (RegsT * RuleLabelT) -> (RegsT * RuleLabelT) -> Prop :=
+  MkCanCombine : forall u1 rm1 ds1 cs1 u2 rm2 ds2 cs2,
+    MF.Disj u1 u2 -> MF.Disj ds1 ds2 -> MF.Disj cs1 cs2 
+  -> notBothRule rm1 rm2
+   -> CanCombine (u1, Build_LabelTP _ rm1 ds1 cs1) 
+                (u2, Build_LabelTP _ rm2 ds2 cs2).
+
+Definition mergeLabel {A : Type} (l1 l2 : LabelTP A) : LabelTP A 
+ := match l1, l2 with
+  Build_LabelTP rm1 ds1 cs1, Build_LabelTP rm2 ds2 cs2 =>
+  Build_LabelTP _ 
+    (match rm1 with | Some r => Some r | None => rm2 end) 
+    (MF.union ds1 ds2) 
+    (MF.union cs1 cs2)
+  end.
+
+Definition equivalent {A : Type} 
+  (l1 l2 : LabelTP A) (p : LabelTP A -> LabelTP A) :=
    l1 = p l2.
 
 Local Open Scope string.
 
-Inductive UnitStep : Modules -> RegsT -> RegsT -> LabelT -> Type :=
+Definition emptyStepName : string := "__empty__".
+
+Inductive UnitStep : Modules -> RegsT -> RegsT -> RuleLabelT -> Type :=
 | EmptyStep : forall
    (regInits : list RegInitT)
    (rules : list (Attribute (Action Void)))
@@ -30,7 +55,8 @@ Inductive UnitStep : Modules -> RegsT -> RegsT -> LabelT -> Type :=
    (ruleMeth : bool),
    MF.InDomain oRegs (namesOf regInits)
    -> UnitStep (Mod regInits rules meths) oRegs (M.empty _)
-        (ruleMeth, M.empty _, M.empty _)
+        (Build_LabelTP _ (if ruleMeth then Some emptyStepName else None)
+         (M.empty _) (M.empty _))
 | SingleRule : 
   forall (ruleName : string) (ruleBody : Action (Bit 0))
    (regInits : list RegInitT)
@@ -42,7 +68,7 @@ Inductive UnitStep : Modules -> RegsT -> RegsT -> LabelT -> Type :=
   SemAction oRegs (ruleBody type) news calls retV ->
    MF.InDomain oRegs (namesOf regInits) ->
   UnitStep (Mod regInits rules meths) oRegs news
-       (true, M.empty _, calls)
+       (Build_LabelTP _ (Some ruleName) (M.empty _) calls)
 | SingleMeth : forall 
    (regInits : list RegInitT)
    (rules : list (Attribute (Action Void)))
@@ -59,25 +85,25 @@ Inductive UnitStep : Modules -> RegsT -> RegsT -> LabelT -> Type :=
                  objVal := (argV, retV) |} (M.empty _) ->
    MF.InDomain oRegs (namesOf regInits) ->
    UnitStep (Mod regInits rules meths) oRegs news
-      (false, udefs, calls)
+      (Build_LabelTP _ None udefs calls)
 | LeftIntro : forall (m1 m2 : Modules)
     (oRegs1 oRegs2 oRegs news : RegsT)
-    (l : LabelT),
+    (l : RuleLabelT),
     MF.InDomain oRegs2 (namesOf (getRegInits m2)) ->
     UnitStep m1 oRegs1 news l ->
     oRegs = MF.union oRegs1 oRegs2 ->
     UnitStep (ConcatMod m1 m2) oRegs news l
 | RightIntro : forall (m1 m2 : Modules)
     (oRegs1 oRegs2 oRegs news : RegsT)
-    (l : LabelT),
+    (l : RuleLabelT),
     MF.InDomain oRegs1 (namesOf (getRegInits m1)) ->
     UnitStep m2 oRegs2 news l ->
     oRegs = MF.union oRegs1 oRegs2 ->
     UnitStep (ConcatMod m1 m2) oRegs news l.
 
-Inductive UnitSteps (m : Modules) (o : RegsT) : RegsT -> LabelT -> Type :=
+Inductive UnitSteps (m : Modules) (o : RegsT) : RegsT -> RuleLabelT -> Type :=
  | UnitSteps1 : forall {u l}, UnitStep m o u l -> UnitSteps m o u l
- | UnitStepsUnion : forall {u1 u2 : RegsT} {l1 l2 : LabelT}, 
+ | UnitStepsUnion : forall {u1 u2 : RegsT} {l1 l2 : RuleLabelT}, 
     UnitSteps m o u1 l1 -> UnitSteps m o u2 l2 
     -> CanCombine (u1, l1) (u2, l2)
     -> UnitSteps m o (MF.union u1 u2) (mergeLabel l1 l2).
@@ -106,22 +132,26 @@ destruct (SignatureT_dec objType objType0).
   assumption.
 Qed.
 
-Definition hide (l : LabelT) : LabelT := match l with
-  (rm, ds, cs) => (rm, subtractKV signIsEq ds cs, subtractKV signIsEq cs ds)
+Definition hide {A : Type} (l : LabelTP A) : LabelTP A 
+  := match l with
+  Build_LabelTP rm ds cs => 
+    Build_LabelTP _ rm (subtractKV signIsEq ds cs)
+                       (subtractKV signIsEq cs ds)
   end.
 
-Definition wellHidden (l : LabelT) (m : Modules) := match l with
-  (rm, ds, cs) =>
-      MF.NotOnDomain ds (getCmsMod m)
-    /\ MF.NotOnDomain cs (getDmsMod m)
+Definition wellHidden {A : Type} (l : LabelTP A) (m : Modules) 
+  := match l with
+    Build_LabelTP rm ds cs =>
+        MF.NotOnDomain ds (getCmsMod m)
+      /\ MF.NotOnDomain cs (getDmsMod m)
   end.
 
-Inductive Step (m : Modules) (o u : RegsT) : LabelT -> Type :=
-  MkStep : forall {l l': LabelT}, UnitSteps m o u l
+Inductive Step (m : Modules) (o u : RegsT) : RuleLabelT -> Type :=
+  MkStep : forall {l l': RuleLabelT}, UnitSteps m o u l
    -> l' = hide l -> wellHidden l' m 
    -> Step m o u l'.
 
-Inductive MultiStep (m : Modules) (regs : RegsT) : RegsT -> list LabelT -> Prop :=
+Inductive MultiStep (m : Modules) (regs : RegsT) : RegsT -> list RuleLabelT -> Prop :=
 | MSNil : MultiStep m regs regs nil
 | MSMulti : forall regs' labels, MultiStep m regs regs' labels
    -> forall l u, Step m regs' u l
@@ -129,9 +159,10 @@ Inductive MultiStep (m : Modules) (regs : RegsT) : RegsT -> list LabelT -> Prop 
    -> MultiStep m regs regs'' (l :: labels).
 
 Inductive Behavior (m : Modules) : RegsT -> list LabelT -> Prop :=
- MkBehavior : forall newRegs labels
+ MkBehavior : forall newRegs labels labels'
    , MultiStep m (initRegs (getRegInits m)) newRegs labels
-   -> Behavior m newRegs labels.
+   -> labels' = (List.map (mapLabelTP (fun _ => I)) labels)
+   -> Behavior m newRegs labels'.
 
 Definition TraceRefines (mimpl mspec : Modules) (f : LabelT -> LabelT)
   := forall simp limp,        Behavior mimpl simp         limp ->
@@ -162,23 +193,33 @@ Section Decomposition.
   Variable imp spec: Modules.
   Variable stateMap: RegsT -> RegsT.
   Variable ruleMap: string -> string.
-  Variable p : LabelT -> LabelT.
+  Variable p : (CallsT * CallsT) -> (CallsT * CallsT).
 
-  Hypothesis pmerge : forall (ds1 ds2 cs1 cs2 : CallsT) (rm1 rm2 : bool)
-    , let l1 := (rm1, ds1, cs1) in let l2 := (rm2, ds2, cs2) in
+  Definition pMod {A : Type} (f : A -> A) (l : LabelTP A) : LabelTP A
+    := match l with
+    | Build_LabelTP rm dm cm => let (dm', cm') := p (dm, cm) in
+       Build_LabelTP _ (option_map f rm) dm' cm'
+    end.
+
+  Definition pRL := pMod ruleMap.
+
+  Hypothesis pmerge : forall (ds1 ds2 cs1 cs2 : CallsT) (rm1 rm2 : option string)
+    , let l1 := Build_LabelTP _ rm1 ds1 cs1 in 
+      let l2 := Build_LabelTP _ rm2 ds2 cs2 in
        MF.Disj ds1 ds2 -> MF.Disj cs1 cs2
-     -> bothNotRule rm1 rm2
-     -> mergeLabel (p l1) (p l2) = p (mergeLabel l1 l2).
+     -> notBothRule rm1 rm2
+     -> mergeLabel (pRL l1) (pRL l2) = pRL (mergeLabel l1 l2).
 
-  Hypothesis phide : forall (l : LabelT), p (hide l) = hide (p l).
+  Hypothesis phide : forall (l : RuleLabelT), pRL (hide l) = hide (pRL l).
 
-  Hypothesis pwellHidden : forall (l : LabelT), 
-   wellHidden (hide l) imp -> wellHidden (p (hide l)) spec.
+  Hypothesis pwellHidden : forall (l : RuleLabelT), 
+   wellHidden (hide l) imp -> wellHidden (pRL (hide l)) spec.
 
-  Variable T : forall {oImp nImp lImp}, UnitStep imp oImp nImp lImp -> (RegsT * LabelT).
+  Variable T : forall {oImp nImp lImp}, UnitStep imp oImp nImp lImp 
+             -> (RegsT * RuleLabelT).
 
   Fixpoint Ts {oImp nImp lImp} (steps : UnitSteps imp oImp nImp lImp)
-      : RegsT * LabelT
+      : RegsT * RuleLabelT
       := match steps with
     | UnitSteps1 _ _ step => T step
     | UnitStepsUnion _ _ _ _ step1 step2 canCombine => 
@@ -188,7 +229,7 @@ Section Decomposition.
     end.
 
   Let Ts' {oImp nImp lImp} (step : Step imp oImp nImp lImp)
-      : RegsT * LabelT
+      : RegsT * RuleLabelT
       := match step with
     | MkStep _ _ steps _ _  => Ts steps
     end.
@@ -209,7 +250,7 @@ Section Decomposition.
    , match T step with (nSpec, lSpec) =>
       let oSpec := stateMap oImp in
         (update oSpec nSpec = stateMap (update oImp nImp)
-      /\ equivalent lSpec lImp p)
+      /\ equivalent lSpec lImp pRL)
       * UnitStep spec oSpec nSpec lSpec
      end.
 
@@ -220,16 +261,25 @@ Section Decomposition.
   , CanCombine (nImp1, lImp1) (nImp2, lImp2)
   -> CanCombine (T step1) (T step2).
 
+  Lemma pBeforeAfter : forall (l : RuleLabelT),
+    pMod id (mapLabelTP (fun _ : string => I) l) =
+     mapLabelTP (fun _ : string => I) (pRL l).
+  Proof. intros. unfold pRL, pMod.
+  destruct l. simpl.
+  destruct (p (dms0, cms0)). simpl.
+  destruct ruleMeth0; simpl; unfold id; reflexivity.
+  Qed.
+
   Lemma canCombineLeft : forall {t u1 u2 l1 l2},
     CanCombine t (MF.union u1 u2, mergeLabel l1 l2)
   -> CanCombine t (u1, l1).
   Proof. 
     intros.
-    destruct l1 as [[rm1 ds1] cs1].
-    destruct l2 as [[rm2 ds2] cs2].
+    destruct l1 as [rm1 ds1 cs1].
+    destruct l2 as [rm2 ds2 cs2].
     inversion H; clear H; subst. 
     constructor; try (eapply MF.Disj_union_1; eassumption).
-    unfold bothNotRule in *. destruct rm1; intuition.
+    unfold notBothRule in *. destruct rm1; intuition.
   Qed.
 
   Lemma canCombineRight : forall {t u1 u2 l1 l2},
@@ -237,11 +287,12 @@ Section Decomposition.
   -> CanCombine t (u2, l2).
   Proof.
     intros.
-    destruct l1 as [[rm1 ds1] cs1].
-    destruct l2 as [[rm2 ds2] cs2].
+    destruct l1 as [rm1 ds1 cs1].
+    destruct l2 as [rm2 ds2 cs2].
     inversion H; clear H; subst. 
     constructor; try (eapply MF.Disj_union_2; eassumption).
-    unfold bothNotRule in *. destruct rm1; intuition.
+    unfold notBothRule in *. destruct rm1; intuition.
+    congruence.
   Qed.
 
   Lemma canCombineMerge : forall {t u1 u2 l1 l2},
@@ -249,13 +300,13 @@ Section Decomposition.
   -> CanCombine t (u2, l2)
   -> CanCombine t (MF.union u1 u2, mergeLabel l1 l2).
   Proof. 
-    intros. destruct t as [uL [[rmL csL] dsL]]. 
-    destruct (mergeLabel l1 l2) as [[rm1 cs] ds] eqn:labeleqn.
+    intros. destruct t as [uL [rmL csL dsL]]. 
+    destruct (mergeLabel l1 l2) as [rm1 cs ds] eqn:labeleqn.
     inversion H; inversion H0; inversion labeleqn; subst.
     econstructor. apply MF.Disj_union; assumption.
     apply MF.Disj_union; assumption.
     apply MF.Disj_union; assumption.
-    unfold bothNotRule in *; destruct rmL, rm2, rm4; intuition.
+    unfold notBothRule in *; destruct rmL, rm2, rm4; intuition.
   Qed.  
 
   Lemma canCombineSym : forall {t1 t2},
@@ -264,7 +315,7 @@ Section Decomposition.
    intros t1 t2 H;
    inversion H; clear H; subst; 
    constructor; eauto using MF.Disj_comm.
-   unfold bothNotRule in *.
+   unfold notBothRule in *.
    destruct rm1, rm2; intuition.
   Qed. 
 
@@ -302,7 +353,7 @@ Require CommonTactics.
       , let (nSpec, lSpec) := Ts steps in
         let oSpec := stateMap oImp in
         (update oSpec nSpec = stateMap (update oImp nImp)
-         /\ equivalent lSpec lImp p)
+         /\ equivalent lSpec lImp pRL)
         * UnitSteps spec oSpec nSpec lSpec.
   Proof.
     intros.
@@ -331,7 +382,7 @@ Require CommonTactics.
       , let (nSpec, lSpec) := Ts' step in
         let oSpec := stateMap oImp in
         (update oSpec nSpec = stateMap (update oImp nImp))
-        * Step spec oSpec nSpec (p lImp).
+        * Step spec oSpec nSpec (pRL lImp).
   Proof. 
     intros.
     destruct step eqn:stepeqn.
@@ -345,18 +396,21 @@ Require CommonTactics.
     apply (pwellHidden _ w).
   Qed. 
 
-  Theorem decomposition : TraceRefines imp spec p.
+  Theorem decomposition : TraceRefines imp spec (pMod id).
   Proof.
     unfold TraceRefines. intros.
     exists (stateMap simp).
     pose proof H.
-    constructor. 
     inversion H0; clear H0; subst.
+    econstructor.
+    Focus 2. instantiate (1 := map pRL labels).
+    repeat rewrite map_map. apply map_ext.
+    intros. apply pBeforeAfter.
     induction H1.
     - rewrite <- stateMapBeginsWell. apply MSNil.
-    - assert (Behavior imp regs' labels).
-      constructor. assumption.
-      pose proof (consistentStepMap (ex_intro _ labels H2) X).
+    - assert (Behavior imp regs' (map (mapLabelTP (fun _ : string => I)) labels)).
+      econstructor. eassumption. reflexivity.
+      pose proof (consistentStepMap (ex_intro _ _ H2) X).
       destruct (Ts' X) eqn:Ts'X.
       destruct X0 as [HT1 HT2].
       apply MSMulti with (regs' := stateMap regs') (u := r).
