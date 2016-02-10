eda7355594c4e8f1d153ac6ebff1f658745acd2a
Murali Vijayaraghavan <vmurali@csail.mit.edu>
2016-02-10 01:35:28 -0500

    added high level theorem regarding renaming, fixed names

--- src/Renaming.v
+++ src/Renaming.v
@@ -45,15 +45,15 @@ Section Rename.
   Variable p1To1: forall x y, p x = p y -> x = y.
   Variable pOnto: forall x, exists y, p y = x.
 
-  Definition mapNamesAttr A a := {| attrName := p (@attrName A a); attrType := attrType a |}.
+  Definition renameAttr A a := {| attrName := p (@attrName A a); attrType := attrType a |}.
   
-  Fixpoint mapNamesList A (ls: list (Attribute A)) :=
-    map (@mapNamesAttr _) ls.
+  Fixpoint renameList A (ls: list (Attribute A)) :=
+    map (@renameAttr _) ls.
 
-  Definition mapNamesMap A (m: M.t A) :=
+  Definition renameMap A (m: M.t A) :=
     M.fold (fun k v old => M.add (p k) v old) m (M.empty _).
 
-  Definition mapNameUnitLabel l :=
+  Definition renameUnitLabel l :=
     match l with
       | Rle None => Rle None
       | Meth None => Meth None
@@ -61,21 +61,21 @@ Section Rename.
       | Meth (Some {| attrName := f; attrType := v |}) => Meth (Some {| attrName := p f; attrType := v |})
     end.
 
-  Definition mapNamesLabel l :=
+  Definition renameLabel l :=
     match l with
     | Build_LabelT annot defs calls =>
       Build_LabelT (match annot with
                     | Some (Some r) => Some (Some (p r))
                     | x => x
-                    end) (mapNamesMap defs) (mapNamesMap calls)
+                    end) (renameMap defs) (renameMap calls)
     end.
   
-  Lemma mapNamesMapEmpty A: mapNamesMap (M.empty A) = M.empty A.
+  Lemma renameMapEmpty A: renameMap (M.empty A) = M.empty A.
   Proof.
     apply (M.F.P.fold_Empty); intuition.
   Qed.
 
-  Lemma mapNamesAdd_transpose_neqkey A:
+  Lemma renameAdd_transpose_neqkey A:
     M.F.P.transpose_neqkey
       eq
       (fun (k0 : M.key) (v0 : A) (old : M.t A) => M.add (p k0) v0 old).
@@ -84,48 +84,48 @@ Section Rename.
     meq; M.cmp (p k) (p k'); findeq.
     elim H; auto.
   Qed.
-  Hint Immediate mapNamesAdd_transpose_neqkey.
+  Hint Immediate renameAdd_transpose_neqkey.
   
-  Lemma mapNamesMapAdd:
+  Lemma renameMapAdd:
     forall {A} m k (v: A),
       (* ~ M.In k m -> *)
-      mapNamesMap (M.add k v m) = M.add (p k) v (mapNamesMap m).
+      renameMap (M.add k v m) = M.add (p k) v (renameMap m).
   Proof.
     intros; remember (M.find k m) as okm. destruct okm.
     - apply eq_sym, M.find_add_3 in Heqokm.
       destruct Heqokm as [sm [? ?]]; subst.
       rewrite M.add_idempotent.
-      unfold mapNamesMap.
+      unfold renameMap.
       rewrite M.F.P.fold_add; auto.
       rewrite M.F.P.fold_add; auto.
-    - unfold mapNamesMap.
+    - unfold renameMap.
       rewrite M.F.P.fold_add; auto.
       apply M.F.P.F.not_find_in_iff; auto.
   Qed.
   
-  Lemma mapNamesMapsTo1 A m: forall k (v: A),
+  Lemma renameMapsTo1 A m: forall k (v: A),
       M.MapsTo k v m ->
-      M.MapsTo (p k) v (mapNamesMap m).
+      M.MapsTo (p k) v (renameMap m).
   Proof.
     M.mind m.
     - apply M.F.P.F.empty_mapsto_iff in H; intuition.
     - apply M.F.P.F.add_mapsto_iff in H1.
       destruct H1 as [[keq veq] | [kneq kin]].
       + subst.
-        rewrite mapNamesMapAdd; intuition.
+        rewrite renameMapAdd; intuition.
         apply M.F.P.F.add_mapsto_iff; intuition.
       + specialize (H _ _ kin).
-        rewrite mapNamesMapAdd; intuition.
+        rewrite renameMapAdd; intuition.
         apply M.F.P.F.add_mapsto_iff; intuition.
   Qed.
 
-  Lemma mapNamesMapsTo2 A m: forall k (v: A),
-      M.MapsTo (p k) v (mapNamesMap m) ->
+  Lemma renameMapsTo2 A m: forall k (v: A),
+      M.MapsTo (p k) v (renameMap m) ->
       M.MapsTo k v m.
   Proof.
     M.mind m.
     - apply M.F.P.F.empty_mapsto_iff in H; intuition.
-    - rewrite mapNamesMapAdd in H1; intuition; apply M.F.P.F.add_mapsto_iff in H1.
+    - rewrite renameMapAdd in H1; intuition; apply M.F.P.F.add_mapsto_iff in H1.
       destruct H1 as [[keq veq] | [kneq kin]].
       + specialize (p1To1 keq).
         subst.
@@ -135,27 +135,27 @@ Section Rename.
         apply M.F.P.F.add_mapsto_iff; intuition.
   Qed.
 
-  Fixpoint mapNamesAction k t (a: ActionT t k) :=
+  Fixpoint renameAction k t (a: ActionT t k) :=
     match a with
-    | MCall meth s e cont => MCall (p meth) s e (fun v => mapNamesAction (cont v))
-    | Let_ lret' e cont => Let_ e (fun v => mapNamesAction (cont v))
-    | ReadReg r k cont => ReadReg r k (fun v => mapNamesAction (cont v))
-    | WriteReg r k e cont => WriteReg r e (mapNamesAction cont)
-    | IfElse e k t f cont => IfElse e (mapNamesAction t) (mapNamesAction f)
-                                   (fun v => mapNamesAction (cont v))
-    | Assert_ e cont => Assert_ e (mapNamesAction cont)
+    | MCall meth s e cont => MCall (p meth) s e (fun v => renameAction (cont v))
+    | Let_ lret' e cont => Let_ e (fun v => renameAction (cont v))
+    | ReadReg r k cont => ReadReg r k (fun v => renameAction (cont v))
+    | WriteReg r k e cont => WriteReg r e (renameAction cont)
+    | IfElse e k t f cont => IfElse e (renameAction t) (renameAction f)
+                                   (fun v => renameAction (cont v))
+    | Assert_ e cont => Assert_ e (renameAction cont)
     | Return e => Return e
     end.
 
-  Definition mapNamesRules (rules: list (Attribute (Action Void))) :=
+  Definition renameRules (rules: list (Attribute (Action Void))) :=
                               map (fun x => match x with
                                          | {| attrName := r;
                                               attrType := a |} =>
                                            {| attrName := p r;
-                                              attrType := fun ty => mapNamesAction (a ty) |}
+                                              attrType := fun ty => renameAction (a ty) |}
                                          end) rules.
 
-  Definition mapNamesMeths (meths: list DefMethT): list DefMethT.
+  Definition renameMeths (meths: list DefMethT): list DefMethT.
     refine (map (fun x => match x with
                        | {| attrName := m;
                             attrType := a |} =>
@@ -163,29 +163,29 @@ Section Rename.
                             attrType := existT _ (projT1 a)
                                                (fun ty v => _) |}
                        end) meths).
-    exact (mapNamesAction (projT2 a ty v)).
+    exact (renameAction (projT2 a ty v)).
   Defined.
 
-  Fixpoint mapNamesModules (m: Modules) :=
+  Fixpoint renameModules (m: Modules) :=
     match m with
-    | Mod regs rules dms => Mod (mapNamesList regs) (mapNamesRules rules) (mapNamesMeths dms)
-    | ConcatMod m1 m2 => ConcatMod (mapNamesModules m1) (mapNamesModules m2)
+    | Mod regs rules dms => Mod (renameList regs) (renameRules rules) (renameMeths dms)
+    | ConcatMod m1 m2 => ConcatMod (renameModules m1) (renameModules m2)
     end.
 
-  Theorem traceRefinesRename m: traceRefines (mapNamesMap (A := _)) m (mapNamesModules m).
+  Theorem traceRefinesRename m: traceRefines (renameMap (A := _)) m (renameModules m).
   Proof.
     admit.
   Qed.
 
   Lemma actionMapName o k a u cs r (sa: @SemAction o k a u cs r):
-    SemAction (mapNamesMap o) (mapNamesAction a) (mapNamesMap u) (mapNamesMap cs) r.
+    SemAction (renameMap o) (renameAction a) (renameMap u) (renameMap cs) r.
   Proof.
     admit.
   Qed.
 
-  Lemma mapNamesKeysEq A (m: M.t A) l:
+  Lemma renameKeysEq A (m: M.t A) l:
     M.KeysEq m l ->
-    M.KeysEq (mapNamesMap m) (map p l).
+    M.KeysEq (renameMap m) (map p l).
   Proof.
     admit.
   Qed.
