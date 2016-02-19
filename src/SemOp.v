53d144fd9f2b4d8fceaa9c9dc09f9699c2db86e9
Joonwon Choi <joonwonc@mit.edu>
2016-02-19 15:29:22 -0500

    Changed the new semantics name

--- src/SemOp.v
+++ src/SemOp.v
@@ -11,7 +11,7 @@ Section GivenModule.
 
   Definition UpdatesT := RegsT.
 
-  Inductive SemActionSmall:
+  Inductive SemActionOp:
     forall k, ActionT type k -> RegsT -> MethsT -> type k -> Prop :=
   | SASMCallExt
       meth s (marg: Expr type (SyntaxKind (arg s)))
@@ -21,8 +21,8 @@ Section GivenModule.
       (HNotIn: ~ In meth (getDefs m))
       newRegs (calls: MethsT) acalls
       (HAcalls: acalls = M.add meth (existT _ _ (evalExpr marg, mret)) calls)
-      (HSemAction: SemActionSmall (cont mret) newRegs calls fret):
-      SemActionSmall (MCall meth s marg cont) newRegs acalls fret
+      (HSemAction: SemActionOp (cont mret) newRegs calls fret):
+      SemActionOp (MCall meth s marg cont) newRegs acalls fret
   | SASMCallInt
       meth methBody
       (marg: Expr type (SyntaxKind (arg (projT1 methBody))))
@@ -31,34 +31,34 @@ Section GivenModule.
       (cont: type (ret (projT1 methBody)) -> ActionT type retK)
       (HIn: In (meth :: methBody)%struct (getDefsBodies m))
       cnewRegs ccalls
-      (HCallee: SemActionSmall ((projT2 methBody) type (evalExpr marg))
+      (HCallee: SemActionOp ((projT2 methBody) type (evalExpr marg))
                                cnewRegs ccalls mret)
       newRegs (calls: MethsT)
-      (HSemAction: SemActionSmall (cont mret) newRegs calls fret)
+      (HSemAction: SemActionOp (cont mret) newRegs calls fret)
       (Hnews: M.Disj cnewRegs newRegs)
       (Hcalls: M.Disj ccalls calls):
-      SemActionSmall (MCall meth (projT1 methBody) marg cont)
+      SemActionOp (MCall meth (projT1 methBody) marg cont)
                      (M.union cnewRegs newRegs) (M.union ccalls calls) fret
   | SASLet
       k (e: Expr type k) retK (fret: type retK)
       (cont: fullType type k -> ActionT type retK) newRegs calls
-      (HSemActionSmall: SemActionSmall (cont (evalExpr e)) newRegs calls fret):
-      SemActionSmall (Let_ e cont) newRegs calls fret
+      (HSemActionOp: SemActionOp (cont (evalExpr e)) newRegs calls fret):
+      SemActionOp (Let_ e cont) newRegs calls fret
   | SASReadReg
       (r: string) regT (regV: fullType type regT)
       retK (fret: type retK) (cont: fullType type regT -> ActionT type retK)
       newRegs calls
       (HRegVal: M.find r oldRegs = Some (existT _ regT regV))
-      (HSemActionSmall: SemActionSmall (cont regV) newRegs calls fret):
-      SemActionSmall (ReadReg r _ cont) newRegs calls fret
+      (HSemActionOp: SemActionOp (cont regV) newRegs calls fret):
+      SemActionOp (ReadReg r _ cont) newRegs calls fret
   | SASWriteReg
       (r: string) k
       (e: Expr type k)
       retK (fret: type retK)
       (cont: ActionT type retK) newRegs calls anewRegs
       (HANewRegs: anewRegs = M.add r (existT _ _ (evalExpr e)) newRegs)
-      (HSemActionSmall: SemActionSmall cont newRegs calls fret):
-      SemActionSmall (WriteReg r e cont) anewRegs calls fret
+      (HSemActionOp: SemActionOp cont newRegs calls fret):
+      SemActionOp (WriteReg r e cont) anewRegs calls fret
   | SASIfElseTrue
       (p: Expr type (SyntaxKind Bool)) k1
       (a: ActionT type k1)
@@ -67,12 +67,12 @@ Section GivenModule.
       k2 (cont: type k1 -> ActionT type k2)
       newRegs1 newRegs2 calls1 calls2 (r2: type k2)
       (HTrue: evalExpr p = true)
-      (HAction: SemActionSmall a newRegs1 calls1 r1)
-      (HSemActionSmall: SemActionSmall (cont r1) newRegs2 calls2 r2)
+      (HAction: SemActionOp a newRegs1 calls1 r1)
+      (HSemActionOp: SemActionOp (cont r1) newRegs2 calls2 r2)
       unewRegs ucalls
       (HUNewRegs: unewRegs = M.union newRegs1 newRegs2)
       (HUCalls: ucalls = M.union calls1 calls2):
-      SemActionSmall (IfElse p a a' cont) unewRegs ucalls r2
+      SemActionOp (IfElse p a a' cont) unewRegs ucalls r2
   | SASIfElseFalse
       (p: Expr type (SyntaxKind Bool)) k1
       (a: ActionT type k1)
@@ -81,71 +81,71 @@ Section GivenModule.
       k2 (cont: type k1 -> ActionT type k2)
       newRegs1 newRegs2 calls1 calls2 (r2: type k2)
       (HFalse: evalExpr p = false)
-      (HAction: SemActionSmall a' newRegs1 calls1 r1)
-      (HSemActionSmall: SemActionSmall (cont r1) newRegs2 calls2 r2)
+      (HAction: SemActionOp a' newRegs1 calls1 r1)
+      (HSemActionOp: SemActionOp (cont r1) newRegs2 calls2 r2)
       unewRegs ucalls
       (HUNewRegs: unewRegs = M.union newRegs1 newRegs2)
       (HUCalls: ucalls = M.union calls1 calls2):
-      SemActionSmall (IfElse p a a' cont) unewRegs ucalls r2
+      SemActionOp (IfElse p a a' cont) unewRegs ucalls r2
   | SASAssertTrue
       (p: Expr type (SyntaxKind Bool)) k2
       (cont: ActionT type k2) newRegs2 calls2 (r2: type k2)
       (HTrue: evalExpr p = true)
-      (HSemActionSmall: SemActionSmall cont newRegs2 calls2 r2):
-      SemActionSmall (Assert_ p cont) newRegs2 calls2 r2
+      (HSemActionOp: SemActionOp cont newRegs2 calls2 r2):
+      SemActionOp (Assert_ p cont) newRegs2 calls2 r2
   | SASReturn
       k (e: Expr type (SyntaxKind k)) evale
       (HEvalE: evale = evalExpr e):
-      SemActionSmall (Return e) (M.empty _) (M.empty _) evale.
+      SemActionOp (Return e) (M.empty _) (M.empty _) evale.
 
-  Theorem inversionSemActionSmall
+  Theorem inversionSemActionOp
           k a news calls retC
-          (evalA: @SemActionSmall k a news calls retC):
+          (evalA: @SemActionOp k a news calls retC):
     match a with
     | MCall meth s e c =>
       (In meth (getDefs m) /\
        exists methBody mret cnewRegs ccalls nnewRegs ncalls
               (Hsig: s = projT1 methBody),
          In (meth :: methBody)%struct (getDefsBodies m) /\
-         SemActionSmall ((projT2 methBody) type
+         SemActionOp ((projT2 methBody) type
                                            match Hsig with
                                              eq_refl => (evalExpr e)
                                            end) cnewRegs ccalls mret /\
-         SemActionSmall (c (match eq_sym Hsig with eq_refl => mret end)) nnewRegs ncalls retC /\
+         SemActionOp (c (match eq_sym Hsig with eq_refl => mret end)) nnewRegs ncalls retC /\
          news = M.union cnewRegs nnewRegs /\
          calls = M.union ccalls ncalls /\
          M.Disj cnewRegs nnewRegs /\
          M.Disj ccalls ncalls) \/
       (~ In meth (getDefs m) /\
        exists mret pcalls,
-         SemActionSmall (c mret) news pcalls retC /\
+         SemActionOp (c mret) news pcalls retC /\
          calls = M.add meth (existT _ _ (evalExpr e, mret)) pcalls)
     | Let_ _ e cont =>
-      SemActionSmall (cont (evalExpr e)) news calls retC
+      SemActionOp (cont (evalExpr e)) news calls retC
     | ReadReg r k c =>
       exists rv,
       M.find r oldRegs = Some (existT _ k rv) /\
-      SemActionSmall (c rv) news calls retC
+      SemActionOp (c rv) news calls retC
     | WriteReg r _ e a =>
       exists pnews,
-      SemActionSmall a pnews calls retC /\
+      SemActionOp a pnews calls retC /\
       news = M.add r (existT _ _ (evalExpr e)) pnews
     | IfElse p _ aT aF c =>
       exists news1 calls1 news2 calls2 r1,
       match evalExpr p with
       | true =>
-        SemActionSmall aT news1 calls1 r1 /\
-        SemActionSmall (c r1) news2 calls2 retC /\
+        SemActionOp aT news1 calls1 r1 /\
+        SemActionOp (c r1) news2 calls2 retC /\
         news = M.union news1 news2 /\
         calls = M.union calls1 calls2
       | false =>
-        SemActionSmall aF news1 calls1 r1 /\
-        SemActionSmall (c r1) news2 calls2 retC /\
+        SemActionOp aF news1 calls1 r1 /\
+        SemActionOp (c r1) news2 calls2 retC /\
         news = M.union news1 news2 /\
         calls = M.union calls1 calls2
       end
     | Assert_ e c =>
-      SemActionSmall c news calls retC /\
+      SemActionOp c news calls retC /\
       evalExpr e = true
     | Return e =>
       retC = evalExpr e /\
@@ -168,16 +168,16 @@ Section GivenModule.
     - destruct (evalExpr p); eauto; try discriminate.
   Qed.
 
-  Inductive SubstepSmall: UpdatesT -> LabelT -> Prop :=
+  Inductive SubstepOp: UpdatesT -> LabelT -> Prop :=
   | SSSEmptyRule:
-      SubstepSmall (M.empty _) emptyRuleLabel
+      SubstepOp (M.empty _) emptyRuleLabel
   | SSSEmptyMeth:
-      SubstepSmall (M.empty _) emptyMethLabel
+      SubstepOp (M.empty _) emptyMethLabel
   | SSSRule:
       forall k (a: Action Void)
              (HInRules: List.In {| attrName := k; attrType := a |} (getRules m))
-             u cs (HAction: SemActionSmall (a type) u cs WO),
-        SubstepSmall u {| annot := Some (Some k);
+             u cs (HAction: SemActionOp (a type) u cs WO),
+        SubstepOp u {| annot := Some (Some k);
                           defs := M.empty _;
                           calls := cs |}
   | SSSMeth:
@@ -185,42 +185,42 @@ Section GivenModule.
              (HIn: In f (getDefsBodies m))
              (HNotIn: ~ In (attrName f) (getCalls m))
              u cs argV retV
-             (HAction: SemActionSmall ((projT2 (attrType f)) type argV) u cs retV),
-        SubstepSmall u {| annot := None;
+             (HAction: SemActionOp ((projT2 (attrType f)) type argV) u cs retV),
+        SubstepOp u {| annot := None;
                           defs := M.add (attrName f) (existT _ _ (argV, retV)) (M.empty _);
                           calls := cs |}.
 
-  Inductive SubstepsSmall: UpdatesT -> LabelT -> Prop :=
+  Inductive SubstepsOp: UpdatesT -> LabelT -> Prop :=
   | SSSNil:
-      SubstepsSmall (M.empty _) emptyMethLabel
+      SubstepsOp (M.empty _) emptyMethLabel
   | SSSCons:
       forall pu pl,
-        SubstepsSmall pu pl ->
+        SubstepsOp pu pl ->
         forall nu nl,
           CanCombineUL pu nu pl nl ->
-          SubstepSmall nu nl ->
-          SubstepsSmall (M.union pu nu) (mergeLabel nl pl).
+          SubstepOp nu nl ->
+          SubstepsOp (M.union pu nu) (mergeLabel nl pl).
 
-  Inductive StepSmall: UpdatesT -> LabelT -> Prop :=
-  | StepSmallIntro: forall u l (HSubSteps: SubstepsSmall u l),
-      StepSmall u l.
+  Inductive StepOp: UpdatesT -> LabelT -> Prop :=
+  | StepOpIntro: forall u l (HSubSteps: SubstepsOp u l),
+      StepOp u l.
 
 End GivenModule.
 
-Ltac invertActionSmall H := apply inversionSemActionSmall in H; simpl in H; dest; try subst.
-Ltac invertActionSmallFirst :=
+Ltac invertActionOp H := apply inversionSemActionOp in H; simpl in H; dest; try subst.
+Ltac invertActionOpFirst :=
   match goal with
-  | [H: SemActionSmall _ _ _ _ _ _ |- _] => invertActionSmall H
+  | [H: SemActionOp _ _ _ _ _ _ |- _] => invertActionOp H
   end.
-Ltac invertActionSmallRep :=
+Ltac invertActionOpRep :=
   repeat
     match goal with
-    | [H: SemActionSmall _ _ _ _ _ _ |- _] => invertActionSmall H
+    | [H: SemActionOp _ _ _ _ _ _ |- _] => invertActionOp H
     | [H: if ?c
           then
-            SemActionSmall _ _ _ _ _ _ /\ _ /\ _ /\ _
+            SemActionOp _ _ _ _ _ _ /\ _ /\ _ /\ _
           else
-            SemActionSmall _ _ _ _ _ _ /\ _ /\ _ /\ _ |- _] =>
+            SemActionOp _ _ _ _ _ _ /\ _ /\ _ /\ _ |- _] =>
       let ic := fresh "ic" in
       (remember c as ic; destruct ic; dest; subst)
     end.
@@ -229,9 +229,9 @@ Section Consistency.
 
   Variable m: Modules.
 
-  Lemma semActionSmall_calls:
+  Lemma semActionOp_calls:
     forall o {retK} (a: ActionT type retK) u cs retv,
-      SemActionSmall m o a u cs retv ->
+      SemActionOp m o a u cs retv ->
       M.KeysDisj cs (getDefs m).
   Proof.
     induction 1; simpl; intros; subst; auto.
@@ -242,39 +242,39 @@ Section Consistency.
     - apply M.KeysDisj_empty.
   Qed.
 
-  Lemma substepSmall_calls:
+  Lemma substepOp_calls:
     forall o u l,
-      SubstepSmall m o u l ->
+      SubstepOp m o u l ->
       M.KeysDisj (calls l) (getDefs m).
   Proof.
     induction 1; simpl; intros.
     - apply M.KeysDisj_empty.
     - apply M.KeysDisj_empty.
-    - eapply semActionSmall_calls; exact HAction.
-    - eapply semActionSmall_calls; exact HAction.
+    - eapply semActionOp_calls; exact HAction.
+    - eapply semActionOp_calls; exact HAction.
   Qed.
 
-  Lemma substepsSmall_calls:
+  Lemma substepsOp_calls:
     forall o u l,
-      SubstepsSmall m o u l ->
+      SubstepsOp m o u l ->
       M.KeysDisj (calls l) (getDefs m).
   Proof.
     induction 1; simpl; intros.
     - apply M.KeysDisj_empty.
-    - apply substepSmall_calls in H1.
+    - apply substepOp_calls in H1.
       destruct nl, pl; simpl in *.
       apply M.KeysDisj_union; auto.
   Qed.
 
-  Lemma stepSmall_calls:
+  Lemma stepOp_calls:
     forall o u l,
-      StepSmall m o u l ->
+      StepOp m o u l ->
       M.KeysDisj (calls l) (getDefs m).
-  Proof. induction 1; eapply substepsSmall_calls; eauto. Qed.
+  Proof. induction 1; eapply substepsOp_calls; eauto. Qed.
 
-  Lemma semActionSmall_implies_substepsInd:
+  Lemma semActionOp_implies_substepsInd:
     forall o {retT} (a: ActionT type retT) u cs retv,
-      SemActionSmall m o a u cs retv ->
+      SemActionOp m o a u cs retv ->
       exists cu ccs ru rds rcs,
         SemAction o a cu ccs retv /\
         SubstepsInd m o ru {| annot:= None; defs:= rds; calls:= rcs |}  /\
@@ -314,9 +314,9 @@ Section Consistency.
       mred; unfold M.Sub; auto.
   Qed.
 
-  Lemma substepSmall_implies_substepsInd:
+  Lemma substepOp_implies_substepsInd:
     forall o u l,
-      SubstepSmall m o u l ->
+      SubstepOp m o u l ->
       wellHidden m l /\
       exists ol, l = hide ol /\ SubstepsInd m o u ol.
   Proof.
@@ -338,9 +338,9 @@ Section Consistency.
         constructor.
     - repeat split; simpl.
       + apply M.KeysDisj_empty.
-      + simpl; eapply semActionSmall_calls; eauto.
+      + simpl; eapply semActionOp_calls; eauto.
         exact HAction.
-      + apply semActionSmall_implies_substepsInd in HAction.
+      + apply semActionOp_implies_substepsInd in HAction.
         destruct HAction as [cu [ccs [ru [rds [rcs ?]]]]]; dest; subst.
         eexists {| annot := Some (Some k) |}.
         split.
@@ -357,8 +357,8 @@ Section Consistency.
     - repeat split; simpl.
       + simpl; apply M.KeysDisj_add; auto.
         apply M.KeysDisj_empty.
-      + simpl; eapply semActionSmall_calls; eauto.
-      + apply semActionSmall_implies_substepsInd in HAction.
+      + simpl; eapply semActionOp_calls; eauto.
+      + apply semActionOp_implies_substepsInd in HAction.
         destruct HAction as [cu [ccs [ru [rds [rcs ?]]]]]; dest; subst.
 
         assert (Hf: ~ M.In f (M.union ccs rcs)).
@@ -378,9 +378,9 @@ Section Consistency.
         * admit. (* map stuff *)
   Qed.
 
-  Lemma substepsSmall_implies_substepsInd:
+  Lemma substepsOp_implies_substepsInd:
     forall o u l,
-      SubstepsSmall m o u l ->
+      SubstepsOp m o u l ->
       wellHidden m l /\
       exists ol, l = hide ol /\ SubstepsInd m o u ol.
   Proof.
@@ -389,8 +389,8 @@ Section Consistency.
        exists emptyMethLabel; repeat split; try (constructor; auto; fail)|].
 
     clear H.
-    apply substepSmall_implies_substepsInd in H1.
-    destruct H1 as [? [sol ?]], IHSubstepsSmall as [? [pol ?]]; dest; subst.
+    apply substepOp_implies_substepsInd in H1.
+    destruct H1 as [? [sol ?]], IHSubstepsOp as [? [pol ?]]; dest; subst.
 
     split.
     - admit. (* nontrivial *)
@@ -400,23 +400,23 @@ Section Consistency.
       + admit. (* nontrivial *)
   Qed.
 
-  Lemma stepSmall_implies_Step:
+  Lemma stepOp_implies_Step:
     forall o u l,
-      StepSmall m o u l -> Step m o u l.
+      StepOp m o u l -> Step m o u l.
   Proof.
     intros; inv H.
     apply step_consistent.
-    apply substepsSmall_implies_substepsInd in HSubSteps.
+    apply substepsOp_implies_substepsInd in HSubSteps.
     dest; subst.
     constructor; auto.
   Qed.
 
-  Lemma stepSmall_consistent:
+  Lemma stepOp_consistent:
     forall o u l,
-      StepSmall m o u l <-> Step m o u l.
+      StepOp m o u l <-> Step m o u l.
   Proof.
     intros; split; intros.
-    - apply stepSmall_implies_Step; auto.
+    - apply stepOp_implies_Step; auto.
     - admit.
   Qed.
     
