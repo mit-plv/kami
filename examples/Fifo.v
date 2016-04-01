15e17de3ceac845a7b60eb926f0c6dbc4f7fbc89
Joonwon Choi <joonwonc@mit.edu>
2016-04-01 17:09:02 -0400

    Caught a bug on the Fifo implementation (while proving invariants)

--- examples/Fifo.v
+++ examples/Fifo.v
@@ -35,7 +35,7 @@ Section Fifo.
      Read enqP <- ^"enqP";
      Read deqP <- ^"deqP";
      Write ^"full" <- $$false;
-     LET next_deqP <- (IF #deqP == $$max_index then $0 else #enqP + $1) :: Bit sz;
+     LET next_deqP <- (IF #deqP == $$max_index then $0 else #deqP + $1) :: Bit sz;
      Write ^"empty" <- (#enqP == #next_deqP);
      Write ^"deqP" <- #next_deqP;
      Ret #elt@[#deqP])%kami.
