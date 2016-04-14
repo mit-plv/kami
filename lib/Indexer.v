5125dcaa67cb7f1d47525e0db4f4d87d87c64c4a
Joonwon Choi <joonwonc@mit.edu>
2016-04-13 20:44:36 -0400

    Added Indexer.v

---
+++ lib/Indexer.v
@@ -0,0 +1,88 @@
+Require Import Bool Ascii String List Eqdep Omega.
+Require Import CommonTactics.
+
+Fixpoint string_of_nat (n: nat) :=
+  match n with
+    | O => "a"%string
+    | S n' => append "a"%string (string_of_nat n')
+  end.
+
+Lemma string_of_nat_length: forall i, String.length (string_of_nat i) = S i.
+Proof. induction i; simpl; intros; auto. Qed.
+
+Definition indexSymbol: string := "__"%string.
+
+Definition withIndex str idx := 
+  append (append (string_of_nat idx) indexSymbol) str.
+
+Theorem withIndex_eq : withIndex = fun str idx =>
+  append (append (string_of_nat idx) indexSymbol) str.
+Proof.
+  reflexivity.
+Qed.
+
+Lemma string_of_nat_index_1:
+  forall i j, j <= i -> get j (string_of_nat i) = Some "a"%char.
+Proof.
+  induction i; simpl; intros.
+  - destruct j; try omega; auto.
+  - destruct j; auto.
+    apply IHi; omega.
+Qed.
+
+Lemma string_of_nat_index_2:
+  forall i j, j > i -> get j (string_of_nat i) = None.
+Proof.
+  induction i; simpl; intros.
+  - destruct j; try omega; auto.
+  - destruct j; try omega; auto.
+    apply IHi; omega.
+Qed.
+
+Lemma append_length:
+  forall s1 s2,
+    String.length (s1 ++ s2) = String.length s1 + String.length s2.
+Proof. induction s1; simpl; intros; auto. Qed.
+
+Lemma withIndex_index_1:
+  forall s i j, j <= i -> get j (withIndex s i) = Some "a"%char.
+Proof.
+  unfold withIndex; intros.
+  rewrite <-append_correct1.
+  - rewrite <-append_correct1.
+    + apply string_of_nat_index_1; auto.
+    + rewrite string_of_nat_length; omega.
+  - rewrite append_length.
+    rewrite string_of_nat_length; omega.
+Qed.
+
+Lemma withIndex_index_2:
+  forall s i, get (S i) (withIndex s i) = Some "_"%char.
+Proof.
+  induction i; simpl; intros; auto.
+Qed.
+
+Lemma withIndex_neq:
+  forall a b i j,
+    i <> j ->
+    withIndex a i <> withIndex b j.
+Proof.
+  intros; destruct (gt_eq_gt_dec i j).
+  - destruct s.
+    + intro Hx.
+      assert (get (S i) (withIndex a i) = get (S i) (withIndex b j)) by (rewrite Hx; auto).
+      rewrite withIndex_index_2 with (i:= i) in H0 by omega.
+      rewrite withIndex_index_1 in H0 by omega.
+      inv H0.
+    + elim H; auto.
+  - intro Hx.
+    assert (get (S j) (withIndex a i) = get (S j) (withIndex b j)) by (rewrite Hx; auto).
+    rewrite withIndex_index_2 with (i:= j) in H0 by omega.
+    rewrite withIndex_index_1 in H0 by omega.
+    inv H0.
+Qed.
+
+Global Opaque withIndex.
+
+Notation "str '__' idx" := (withIndex str idx) (at level 0).
+
