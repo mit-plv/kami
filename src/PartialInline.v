75e58dffc7e6a872af00c5b7af008ddd43f648dd
Joonwon Choi <joonwonc@mit.edu>
2016-05-23 16:24:39 -0400

    Proved the first lemma of partial inlining; FMap a bit enhanced

--- src/PartialInline.v
+++ src/PartialInline.v
@@ -17,18 +17,63 @@ Proof.
     simpl; f_equal; auto.
 Qed.
 
+Lemma substepsInd_rule_split:
+  forall m o u l,
+    SubstepsInd m o u l ->
+    forall or,
+      annot l = Some or ->
+      exists ru rcs pu pl,
+        Substep m o ru (Rle or) rcs /\
+        SubstepsInd m o pu pl /\
+        CanCombineUUL pu pl ru rcs (Rle or) /\
+        u = M.union pu ru /\
+        l = mergeLabel (getLabel (Rle or) rcs) pl.
+Proof.
+  induction 1; simpl; intros; [inv H|].
+
+  subst; destruct sul as [|].
+
+  - clear IHSubstepsInd.
+    exists su, scs, u, l.
+
+    destruct l as [ann ds cs]; inv H1; dest; simpl in *.
+    destruct ann; [inv H3|].
+    inv H4.
+    repeat split; auto.
+
+  - clear H.
+    destruct l as [ann ds cs]; simpl in *; subst.
+    specialize (IHSubstepsInd _ eq_refl).
+    destruct IHSubstepsInd as [ru [rcs [pu [pl ?]]]]; dest; subst.
+
+    exists ru, rcs, (M.union pu su), (mergeLabel (getLabel (Meth o0) scs) pl).
+    
+    destruct pl as [pann pds pcs]; inv H1; inv H3; dest; simpl in *.
+    destruct pann as [|]; [inv H7|]; inv H5.
+
+    repeat split; auto.
+    + econstructor.
+      * exact H2.
+      * exact H0.
+      * repeat split; auto.
+      * reflexivity.
+      * reflexivity.
+    + simpl; auto.
+    + f_equal; auto.
+      
+Qed.
+
 (* Partial inlining interfaces *)
 Section Partial.
   Variable m: Modules.
 
   Variable dm: DefMethT. (* a method to be inlined *)
-  Hypotheses (Hdm: In dm (getDefsBodies m)).
-             (* (HnoCallDm: noCallDm dm dm = true) *)
-             (* (HnoDupMeths: NoDup (namesOf (getDefsBodies m))). *)
+  Hypotheses (Hdm: In dm (getDefsBodies m))
+             (HnoDupMeths: NoDup (namesOf (getDefsBodies m))).
   Variable r: Attribute (Action Void). (* a rule calling dm *)
   Hypothesis Hrule: In r (getRules m).
 
-  Lemma inlineDmToRule_substepsInd_intact:
+  Lemma inlineDmToRule_substepsInd_intact_1:
     forall o u l,
       SubstepsInd m o u l ->
       ~ (annot l = Some (Some (attrName r)) /\ M.find (attrName dm) (calls l) <> None) ->
@@ -98,7 +143,53 @@ Section Partial.
 
   Qed.
 
-  Lemma inlineDmToRule_substepsInd_sub_1:
+  Lemma inlineDmToRule_substepsInd_intact_2:
+    forall o pu pds pcs,
+      SubstepsInd m o pu {| annot := None; defs := pds; calls := pcs |} ->
+      forall ru rcs,
+        Substep m o ru (Rle (Some (attrName r))) rcs ->
+        None = M.find (elt:=sigT SignT) (attrName dm) rcs ->
+        (* Some s = M.find (elt:=sigT SignT) (attrName dm) pcs -> *)
+        M.Disj ru pu -> M.Disj rcs pcs ->
+        SubstepsInd
+          (Mod (getRegInits m)
+               (map
+                  (fun newr =>
+                     if string_dec (attrName r) (attrName newr)
+                     then inlineDmToRule newr dm else newr)
+                  (getRules m)) (getDefsBodies m)) o (M.union pu ru)
+          {| annot := Some (Some (attrName r)); defs := pds; calls := M.union rcs pcs |}.
+  Proof.
+    intros; econstructor.
+
+    - apply inlineDmToRule_substepsInd_intact_1.
+      + eassumption.
+      + simpl; intro Hx; dest; discriminate.
+
+    - instantiate (1:= rcs).
+      instantiate (1:= Rle (Some (attrName r))).
+      instantiate (1:= ru).
+      inv H0.
+
+      econstructor.
+      + simpl.
+        apply in_map with (f:= fun newr =>
+                                 if string_dec (attrName r) (attrName newr)
+                                 then inlineDmToRule newr dm
+                                 else newr) in HInRules.
+        simpl in HInRules.
+        destruct (string_dec (attrName r) (attrName r)); [clear e|elim n; reflexivity].
+        eauto.
+      + simpl; destruct dm as [dmn dmb].
+        apply inlineDm_SemAction_intact; auto.
+
+    - repeat split; auto.
+    - reflexivity.
+    - reflexivity.
+
+  Qed.
+
+  Lemma inlineDmToRule_substepsInd_sub:
     forall o u su scs s l,
       Substep m o su (Rle (Some (attrName r))) scs ->
       M.find (elt:=sigT SignT) (attrName dm) scs = Some s ->
@@ -114,7 +205,10 @@ Section Partial.
                      if string_dec (attrName r) (attrName newr)
                      then inlineDmToRule newr dm else newr)
                   (getRules m)) (getDefsBodies m)) o (M.union u su)
-          {| annot := Some (Some (attrName r)); defs := ds; calls := M.union scs cs |}.
+          (hideMeth 
+             {| annot := Some (Some (attrName r));
+                defs := ds;
+                calls := M.union scs cs |} (attrName dm)).
   Proof.
     induction 3; simpl; intros; [exfalso; inv H1; mred|].
 
@@ -143,188 +237,72 @@ Section Partial.
         inv H2; [constructor|].
         econstructor; eauto.
       + inv H6; inv H3; dest; simpl in *.
+        unfold hideMeth; simpl.
+        rewrite <-Heqodp.
+        rewrite M.find_union, H0.
+        destruct (signIsEq _ _); [|elim n; reflexivity].
         repeat split; simpl; auto.
         destruct sul as [|[|]]; auto.
-        inv H5.
+        * inv H5.
+        * findeq; auto.
       + meq.
-      + clear -H6 H8; inv H6.
-        simpl; f_equal.
-        * destruct sul as [|[|]]; auto; inv H0.
-        * meq.
-
+      + unfold hideMeth at 1; simpl.
+        rewrite H9.
+        rewrite M.find_union, H0.
+        destruct (signIsEq _ _); [clear e|elim n; reflexivity].
+        unfold hideMeth; simpl.
+        rewrite <-Heqodp.
+        rewrite M.find_union, H0.
+        destruct (signIsEq _ _); [clear e|elim n; reflexivity].
+        f_equal.
+        * inv H6; destruct sul as [|[|]]; auto; inv H5.
+        * inv H6; inv H3; dest; simpl in *.
+          destruct sul as [|[[dmn dmb]|]]; auto.
+          destruct (string_dec (attrName dm) dmn); auto.
+          subst; meq.
+          simpl in H6; mcontra.
+        * inv H6; inv H3; dest; simpl in *.
+          apply eq_sym in H0; meq.
+          
     - clear IHSubstepsInd.
-      admit.
-
-  Qed.
-
-  Lemma inlineDmToRule_substepsInd_sub_2:
-    forall o u ds cs su scs s l,
-      l = {| annot := Some (Some (attrName r)); defs := ds; calls := cs |} ->
-      SubstepsInd m o u l ->
-      M.find (elt:=sigT SignT) (attrName dm) cs = Some s ->
-      Substep m o su (Meth (Some (attrName dm :: s)%struct)) scs ->
-      M.Disj su u -> M.Disj scs cs ->
-      ~ M.In (elt:=sigT SignT) (attrName dm) ds ->
-      SubstepsInd
-        (Mod (getRegInits m)
-             (map
-                (fun newr =>
-                   if string_dec (attrName r) (attrName newr)
-                   then inlineDmToRule newr dm else newr)
-                (getRules m)) (getDefsBodies m)) o (M.union u su)
-        {| annot := Some (Some (attrName r));
-           defs := M.union (M.add (attrName dm) s (M.empty (sigT SignT))) ds;
-           calls := M.union scs cs |}.
-  Proof.
-    induction 2; simpl; intros; [exfalso; inv H; mred|].
-    admit.
-  Qed.
-
-  Lemma inlineDmToRule_substepsInd:
-    forall o u l,
-      SubstepsInd m o u l ->
-      M.find (attrName dm) (calls l) = None \/
-      M.find (attrName dm) (defs l) = M.find (attrName dm) (calls l) ->
-      SubstepsInd
-        (Mod (getRegInits m)
-             (map
-                (fun newr =>
-                   if string_dec (attrName r) (attrName newr)
-                   then inlineDmToRule newr dm
-                   else newr) (getRules m)) (getDefsBodies m)) o u l.
-  Proof.
-    intros.
-    assert ((annot l = Some (Some (attrName r)) /\ M.find (attrName dm) (calls l) <> None) \/
-            ~ (annot l = Some (Some (attrName r)) /\ M.find (attrName dm) (calls l) <> None)).
-    { clear; destruct l as [a d c]; simpl.
-      destruct a; [|right; intro Hx; dest; discriminate].
-      destruct o; [|right; intro Hx; dest; discriminate].
-      destruct (string_dec s (attrName r));
-        [subst|right; intro Hx; dest; inv H; elim n; reflexivity].
-      destruct (M.find (attrName dm) c).
-      { left; split; [auto|discriminate]. }
-      { right; intro Hx; dest; elim H0; reflexivity. }
-    }
-
-    destruct H1; [|apply inlineDmToRule_substepsInd_intact; auto].
-
-    dest; destruct H0; [elim H2; auto|].
-    remember (M.find (attrName dm) (calls l)) as odml; destruct odml; [|elim H2; reflexivity].
-    apply eq_sym in Heqodml; clear H2.
-
-    induction H; [constructor|subst].
-
-    destruct sul as [or|odm].
-
-    - destruct l as [ann ds cs]; simpl in *.
-      assert (or = Some (attrName r)) by (destruct ann; inv H1; auto).
-      subst; clear H1.
-      inv H3; dest; simpl in *.
-      destruct ann; [inv H4|clear H4].
-
-      clear IHSubstepsInd.
-
-      remember (M.find (attrName dm) scs) as ods; destruct ods.
-
-      + mred; eapply inlineDmToRule_substepsInd_sub_1; eauto.
-
-      + mred.
-        econstructor.
-        * apply inlineDmToRule_substepsInd_intact.
-          { eassumption. }
-          { simpl; intro Hx; dest; inv H4. }
-        * inv H2; eapply SingleRule.
-          { simpl.
-            apply in_map with (f:= fun newr =>
-                                     if string_dec (attrName r) (attrName newr)
-                                     then inlineDmToRule newr dm
-                                     else newr) in HInRules.
-            simpl in HInRules.
-            destruct (string_dec (attrName r) (attrName r)); [|elim n; reflexivity].
-            unfold inlineDmToRule in HInRules; simpl in HInRules.
-            eauto.
-          }
-          { simpl; destruct dm as [dmn dmb].
-            eapply inlineDm_SemAction_intact; eauto.
-          }
-        * repeat split; auto.
-        * reflexivity.
-        * reflexivity.
-
-    - destruct l as [ann ds cs]; simpl in *; subst.
-      assert ((odm = Some ((attrName dm) :: s)%struct /\ M.find (attrName dm) cs = Some s) \/
-              ~ (odm = Some ((attrName dm) :: s)%struct /\ M.find (attrName dm) cs = Some s)).
-      { destruct odm; [|right; intro Hx; dest; discriminate].
-        destruct a as [dmn dmb].
-        destruct (string_dec (attrName dm) dmn);
-          [|right; intro Hx; dest; inv H1; elim n; reflexivity].
-        subst.
-        rewrite M.find_union, M.find_add_1 in H0; inv H0.
-        remember (M.find (attrName dm) cs) as odc; destruct odc;
-          [|right; intro Hx; dest; discriminate].
-        rewrite M.find_union in Heqodml.
-        remember (M.find (attrName dm) scs) as odsc; destruct odsc;
-          [inv H3; dest; simpl in *; mcontra|].
-        rewrite Heqodml in Heqodc; inv Heqodc.
-        left; split; auto.
+      assert (sul = Meth (Some (attrName dm :: s)%struct)); subst.
+      { destruct sul as [|]; inv H6.
+        destruct o0 as [[dmn dmb]|]; [|mred].
+        destruct (string_dec (attrName dm) dmn); subst; mred.
       }
-      destruct H1.
+      inv H3; inv H6; dest; simpl in *; clear H5 H9.
 
-      + dest; subst; simpl in *.
-        clear IHSubstepsInd.
-        clear Heqodml; mred.
-        inv H3; dest; simpl in *.
-        eapply inlineDmToRule_substepsInd_sub_2; eauto.
+      econstructor.
+      + apply inlineDmToRule_substepsInd_intact_1.
+        * eassumption.
+        * simpl; intro Hx; dest; discriminate.
+      + instantiate (1:= M.remove (attrName dm) (M.union scs scs0)).
+        instantiate (1:= Rle (Some (attrName r))).
+        instantiate (1:= M.union su su0).
 
-      + remember (M.find (attrName dm) cs) as odc; destruct odc.
+        inv H; inv H2.
+        econstructor.
+        * simpl.
+          apply in_map with (f:= fun newr =>
+                                   if string_dec (attrName r) (attrName newr)
+                                   then inlineDmToRule newr dm
+                                   else newr) in HInRules.
+          simpl in HInRules.
+          destruct (string_dec _ _); [|elim n; reflexivity].
+          eauto.
+        * simpl; inv Hsig.
+          assert (dm = f) by (eapply in_NoDup_attr; eauto); subst.
+          rewrite M.union_comm with (m1:= su) by auto.
+          replace (M.remove (attrName f) (M.union scs scs0)) with
+          (M.union scs0 (M.remove (attrName f) scs))
+            by (meq; apply eq_sym in H0; mcontra).
+          eapply inlineDm_correct_SemAction; eauto.
 
-        * assert (s = s0); subst.
-          { mred; remember (M.find (attrName dm) scs) as odsc; destruct odsc.
-            { exfalso; inv H3; dest; simpl in *; mcontra. }
-            { inv Heqodml; reflexivity. }
-          }
-          econstructor.
-          { apply IHSubstepsInd; auto.
-            destruct odm as [|]; auto.
-            destruct a as [dmn dmb].
-            destruct (string_dec (attrName dm) dmn).
-            { exfalso; subst.
-              elim H1; clear H1; split; auto.
-              repeat f_equal.
-              findeq.
-            }
-            { findeq. }
-          }
-          { instantiate (1:= scs); instantiate (1:= Meth odm); instantiate (1:= su).
-            inv H2.
-            { eapply EmptyMeth. }
-            { eapply SingleMeth; eauto. }
-          }
-          { inv H3; dest; simpl in *.
-            repeat split; auto.
-          }
-          { reflexivity. }
-          { reflexivity. }
+      + repeat split; auto.
+      + meq.
+      + unfold hideMeth; simpl; mred.
+        simpl; f_equal; meq.
 
-        * clear IHSubstepsInd.
-          econstructor.
-          { apply inlineDmToRule_substepsInd_intact.
-            { eassumption. }
-            { simpl; intro Hx; dest.
-              rewrite <-Heqodc in H5; elim H5; reflexivity.
-            }
-          }
-          { instantiate (1:= scs); instantiate (1:= Meth odm); instantiate (1:= su).
-            inv H2.
-            { eapply EmptyMeth. }
-            { eapply SingleMeth; eauto. }
-          }
-          { inv H3; dest; simpl in *.
-            repeat split; auto.
-          }
-          { reflexivity. }
-          { reflexivity. }
-            
   Qed.
 
   Lemma inlineDmToRule_wellHidden:
@@ -377,13 +355,68 @@ Section Partial.
                    then inlineDmToRule newr dm else newr)
                 (getRules m)) (getDefsBodies m)) o u l.
   Proof.
-    intros; inv H; constructor.
+    intros; inv H.
 
-    - apply wellHidden_find with (a:= attrName dm) in HWellHidden;
-        [|apply in_map; auto].
-      apply inlineDmToRule_substepsInd; auto.
-      
-    - apply inlineDmToRule_wellHidden; auto.
+    destruct l0 as [ann ds cs].
+    assert (ann = Some (Some (attrName r)) \/ ~ ann = Some (Some (attrName r))).
+    { destruct ann; [|right; discriminate].
+      destruct o0; [|right; discriminate].
+      destruct (string_dec s (attrName r)).
+      { subst; left; reflexivity. }
+      { right; intro Hx; inv Hx; elim n; reflexivity. }
+    }
+
+    destruct H.
+
+    - subst.
+      pose proof (substepsInd_rule_split _ _ _ _ HSubSteps) as Hsp.
+      clear HSubSteps.
+      specialize (Hsp _ eq_refl).
+      destruct Hsp as [ru [rcs [pu [pl ?]]]]; dest; subst.
+
+      remember (M.find (attrName dm) rcs) as odr; destruct odr.
+
+      + rewrite <-hideMeth_preserves_hide with (dm:= attrName dm).
+        constructor.
+        * destruct pl as [pann pds pcs]; inv H3.
+          inv H1; dest; simpl in *; mred.
+          destruct pann; [inv H3|].
+          eapply inlineDmToRule_substepsInd_sub; eauto.
+
+          clear -Hdm Heqodr HWellHidden.
+          apply wellHidden_find with (a:= attrName dm) in HWellHidden.
+          { simpl in *; destruct HWellHidden; mred. }
+          { apply in_map; auto. }
+          
+        * rewrite hideMeth_preserves_hide.
+          apply inlineDmToRule_wellHidden; auto.
+
+      + destruct pl as [pann pds pcs]; simpl in *.
+        remember (M.find (attrName dm) pcs) as odp; destruct odp.
+
+        * econstructor.
+          { mred; inv H3; clear H4.
+            inv H1; dest; simpl in *.
+            destruct pann; [inv H3|].
+            apply inlineDmToRule_substepsInd_intact_2; auto.
+          }
+          { apply inlineDmToRule_wellHidden; auto. }
+
+        * econstructor.
+          { apply inlineDmToRule_substepsInd_intact_1; auto.
+            { econstructor; eauto. }
+            { simpl; intro Hx; dest.
+              elim H4; clear H4.
+              inv H3; findeq.
+            }
+          }
+          { apply inlineDmToRule_wellHidden; auto. }
+
+    - econstructor.
+      + apply inlineDmToRule_substepsInd_intact_1; auto.
+        simpl; intro Hx; dest; elim H; auto.
+      + apply inlineDmToRule_wellHidden; auto.
+
   Qed.
 
   Lemma inlineDmToRule_traceRefines_1:
