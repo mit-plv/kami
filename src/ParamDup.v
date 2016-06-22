9d20f728c78d82c8262655d4267ddb05fbf58d3b
Joonwon Choi <joonwonc@mit.edu>
2016-06-22 11:30:20 -0400

    Connecting duplication and repetition, done

--- src/ParamDup.v
+++ src/ParamDup.v
@@ -66,6 +66,30 @@ Proof.
     + simpl; apply in_or_app; right; auto.
 Qed.
 
+Lemma renameAction_specializer_dms:
+  forall regs rules dms i dmn (dm: sigT MethodT) ty v,
+    In (dmn :: dm)%struct dms ->
+    Specializable (Mod regs rules dms) ->
+    Wf.ValidRegsAction (namesOf regs) (projT2 dm ty v) ->
+    ActionEquiv (projT2 dm ty v) (projT2 dm typeUT tt) ->
+    renameAction (specializer (Mod regs rules dms) i) (projT2 dm ty v) =
+    renameAction (spf i) (projT2 dm ty v).
+Proof.
+  intros; apply renameAction_spDom_weakening with
+          (regs:= namesOf regs) (au:= projT2 dm typeUT tt); auto.
+  intros; apply specializer_dom; auto.
+  apply in_app_or in H3; destruct H3.
+  - unfold spDom; apply makeNoDup_SubList_2.
+    apply in_or_app; left; auto.
+  - unfold spDom; apply makeNoDup_SubList_2.
+    do 4 (apply in_or_app; right); simpl.
+
+    clear -H H3; induction dms; [inv H|].
+    inv H.
+    + simpl; apply in_or_app; left; auto.
+    + simpl; apply in_or_app; right; auto.
+Qed.
+
 Section SinModuleDup.
 
   Lemma Specializable_weakening:
@@ -118,6 +142,49 @@ Section SinModuleDup.
         * specialize (H2 ty); inv H2; auto.
         * specialize (H1 ty); inv H1; auto.
     - apply IHrules; auto.
+      + intros; specialize (H1 ty); inv H1; auto.
+      + intros; specialize (H2 ty); inv H2; auto.
+      + eapply SubList_cons_inv; eauto.
+      + eapply SubList_cons_inv; eauto.
+  Qed.
+
+  Lemma specializer_weakening_dms:
+    forall regs rules edms1 edms2 i dms,
+      Specializable (Mod regs rules edms1) ->
+      Specializable (Mod regs rules edms2) ->
+      (forall ty, MethsEquiv ty typeUT dms) ->
+      (forall ty, ValidRegsDms ty (namesOf regs) dms) ->
+      SubList dms edms1 ->
+      SubList dms edms2 ->
+      renameMeths (specializer (Mod regs rules edms1) i) dms =
+      renameMeths (specializer (Mod regs rules edms2) i) dms.
+  Proof.
+    induction dms; simpl; intros; auto.
+    f_equal.
+    - destruct a as [an ar]; simpl; f_equal.
+      + rewrite specializer_dom; auto.
+        * rewrite specializer_dom; auto.
+          unfold spDom; apply makeNoDup_SubList_2.
+          apply in_or_app; right.
+          apply in_or_app; right.
+          apply in_or_app; left; simpl.
+          apply SubList_map with (f:= @attrName _) in H4; simpl in H4.
+          apply H4; left; auto.
+        * unfold spDom; apply makeNoDup_SubList_2.
+          apply in_or_app; right.
+          apply in_or_app; right.
+          apply in_or_app; left; simpl.
+          apply SubList_map with (f:= @attrName _) in H3; simpl in H3.
+          apply H3; left; auto.
+      + f_equal; extensionality ty; extensionality v.
+        repeat (rewrite renameAction_specializer_dms with (dmn:= an); auto).
+        * apply SubList_cons_inv in H4; dest; auto.
+        * specialize (H2 ty); inv H2; auto.
+        * specialize (H1 ty); inv H1; auto.
+        * apply SubList_cons_inv in H3; dest; auto.
+        * specialize (H2 ty); inv H2; auto.
+        * specialize (H1 ty); inv H1; auto.
+    - apply IHdms; auto.
       + intros; specialize (H1 ty); inv H1; auto.
       + intros; specialize (H2 ty); inv H2; auto.
       + eapply SubList_cons_inv; eauto.
@@ -382,6 +449,169 @@ Section SinModuleDup.
         rewrite <-app_assoc; apply SubList_app_2, SubList_refl.
   Qed.
 
+  Lemma methsToRep_duplicate_meths_equiv:
+    forall regs rules {genK} (getConst: nat -> ConstT genK) n (l: list SinMeth),
+      (forall ty, MethsEquiv ty typeUT
+                             (map
+                                (fun sd => ((nameVal (methName sd))
+                                              :: getMethFromSin (methGen sd))%struct) 
+                                l)) ->
+      (forall ty, ValidRegsDms ty (namesOf regs)
+                               (map
+                                  (fun sd => ((nameVal (methName sd))
+                                                :: getMethFromSin (methGen sd))%struct) 
+                                  l)) ->
+      Specializable
+        (Mod regs rules
+             (map
+                (fun sd => ((nameVal (methName sd))
+                              :: getMethFromSin (methGen sd))%struct) 
+                l)) ->
+      EquivList
+        (concat
+           (map getListFromMetaMeth
+                (methsToRep string_of_nat string_of_nat_into 
+                            getConst withIndex_index_eq 
+                            (getNatListToN_NoDup n) 
+                            l)))
+        (getDefsBodies
+           (duplicate
+              (Mod regs rules
+                   (map
+                      (fun sd => (nameVal (methName sd) :: getMethFromSin (methGen sd))%struct) 
+                      l)) n)).
+  Proof.
+    intros; induction l; [simpl; induction n; [apply EquivList_nil|auto]|].
+    destruct a as [arg arn]; simpl.
+
+    assert (EquivList
+              ((repMeth string_of_nat getConst
+                        (existT (fun sig : SignatureT => GenMethodT genK sig) 
+                                (projT1 arg)
+                                (fun (ty : Kind -> Type) (argv : ty (Syntax.arg (projT1 arg))) =>
+                                   convSinToGen true genK (projT2 arg ty argv)))
+                        (nameVal arn) (getNatListToN n))
+                 ++ (getDefsBodies
+                       (duplicate
+                          (Mod regs rules
+                               (map
+                                  (fun sd => ((nameVal (methName sd))
+                                                :: getMethFromSin (methGen sd))%struct) l)) n)))
+              (getDefsBodies
+                 (duplicate
+                    (Mod regs rules
+                         (((nameVal arn :: getMethFromSin arg)%struct)
+                            :: map (fun sd => ((nameVal (methName sd))
+                                                 :: getMethFromSin (methGen sd))%struct) l)) n))).
+    { clear -H H0 H1.
+      simpl in H; generalize dependent H.
+      simpl in H0; generalize dependent H0.
+      simpl in H1; generalize dependent H1.
+      generalize (map (fun sd => ((nameVal (methName sd))
+                                    :: getMethFromSin (methGen sd))%struct) l) as ml.
+      intros.
+      assert (Specializable (Mod regs rules ml)).
+      { eapply Specializable_weakening; eauto; try (unfold getDefs; simpl; apply SubList_refl).
+        { simpl; apply SubList_cons_right, SubList_refl. }
+        { unfold getCalls; simpl.
+          apply SubList_app_6.
+          { apply SubList_refl. }
+          { apply SubList_app_2, SubList_refl. }
+        }
+      }
+      generalize dependent n; induction n.
+      
+      - simpl; apply EquivList_cons.
+        + rewrite specializer_weakening_dms with
+          (edms1:= (nameVal arn :: getMethFromSin arg)%struct :: ml)
+            (edms2:= ml); auto.
+          * apply EquivList_refl.
+          * intros; specialize (H ty); inv H; auto.
+          * intros; specialize (H0 ty); inv H0; auto.
+          * apply SubList_cons_right, SubList_refl.
+          * apply SubList_refl.
+        + f_equal.
+          * rewrite specializer_dom; auto.
+            apply makeNoDup_SubList_2.
+            apply in_or_app; right.
+            apply in_or_app; right.
+            apply in_or_app; left.
+            simpl; auto.
+          * unfold getMethFromGen; simpl; f_equal.
+            extensionality ty; extensionality v.
+            change (getSinAction (projT2 arg ty v)) with (projT2 (getMethFromSin arg) ty v).
+            change (@projT1 SignatureT SinMethodT arg) with
+            (@projT1 SignatureT MethodT (getMethFromSin arg)).
+            rewrite renameAction_specializer_dms with (dmn:= nameVal arn); auto.
+            { apply getGenAction_convSinToGen_renameAction. }
+            { left; auto. }
+            { specialize (H0 ty); inv H0; auto. }
+            { specialize (H ty); inv H; auto. }
+            
+      - generalize dependent IHn.
+        unfold duplicate at 3. fold (duplicate (Mod regs rules ml)).
+        generalize (duplicate (Mod regs rules ml) n) as nm1.
+        unfold duplicate at 2.
+        fold (duplicate
+                (Mod regs rules ((nameVal arn :: getMethFromSin arg)%struct :: ml))).
+        generalize (duplicate
+                      (Mod regs rules ((nameVal arn :: getMethFromSin arg)%struct :: ml)) n) as nm2.
+        simpl.
+        remember (repMeth _ _ _ _ _) as rr1; clear Heqrr1.
+        rewrite specializer_weakening_dms with
+        (edms1:= (nameVal arn :: getMethFromSin arg)%struct :: ml)
+          (edms2:= ml); auto.
+        5:(apply SubList_refl).
+        4:(apply SubList_cons_right, SubList_refl).
+        3:(intros; specialize (H0 ty); inv H0; auto).
+        2:(intros; specialize (H ty); inv H; auto).
+
+        generalize (renameMeths (specializer (Mod regs rules ml) (S n)) ml) as rr2.
+        intros.
+
+        apply EquivList_cons.
+        + inv IHn; split.
+          * repeat apply SubList_app_3.
+            { eapply SubList_app_2, SubList_app_4; eauto. }
+            { apply SubList_app_1, SubList_refl. }
+            { eapply SubList_app_2, SubList_app_5; eauto. }
+          * repeat apply SubList_app_3.
+            { apply SubList_app_2, SubList_app_1, SubList_refl. }
+            { apply SubList_app_comm; rewrite <-app_assoc.
+              apply SubList_app_2, SubList_app_comm; auto.
+            }
+        + rewrite specializer_dom; auto.
+          * f_equal; simpl.
+            unfold getMethFromGen; simpl.
+            f_equal; extensionality ty; extensionality v.
+            change (getSinAction (projT2 arg ty v)) with (projT2 (getMethFromSin arg) ty v).
+            change (@projT1 SignatureT SinMethodT arg) with
+            (@projT1 SignatureT MethodT (getMethFromSin arg)).
+            rewrite renameAction_specializer_dms with (dmn:= nameVal arn); auto.
+            { apply getGenAction_convSinToGen_renameAction. }
+            { left; auto. }
+            { specialize (H0 ty); inv H0; auto. }
+            { specialize (H ty); inv H; auto. }
+          * simpl; unfold spDom; apply makeNoDup_SubList_2.
+            apply in_or_app; right.
+            apply in_or_app; right.
+            apply in_or_app; left.
+            left; auto.
+    }
+
+    eapply EquivList_trans; [|apply H2]; clear H2.
+    apply EquivList_app; [apply EquivList_refl|].
+    apply IHl.
+    - intros; specialize (H ty); inv H; auto.
+    - intros; specialize (H0 ty); inv H0; auto.
+    - clear -H1.
+      eapply Specializable_weakening; eauto; try (unfold getDefs; simpl; apply SubList_refl).
+      + simpl; apply SubList_cons_right, SubList_refl.
+      + unfold getCalls; simpl.
+        apply SubList_app_6; [apply SubList_refl|].
+        apply SubList_app_2, SubList_refl.
+  Qed.
+  
   Variable sm: SinModule nat.
   Hypotheses
     (* Hsp is provable generally, but fine to prove for each instantiation *)
@@ -407,7 +637,9 @@ Section SinModuleDup.
     - apply rulesToRep_duplicate_rules_equiv; auto.
       + intros; specialize (Hequiv ty); inv Hequiv; auto.
       + intros; specialize (Hvr ty); inv Hvr; auto.
-    - admit.
+    - apply methsToRep_duplicate_meths_equiv; auto.
+      + intros; specialize (Hequiv ty); inv Hequiv; auto.
+      + intros; specialize (Hvr ty); inv Hvr; auto.
   Qed.
 
   Lemma sinModule_duplicate_2:
@@ -426,7 +658,9 @@ Section SinModuleDup.
     - apply EquivList_comm, rulesToRep_duplicate_rules_equiv; auto.
       + intros; specialize (Hequiv ty); inv Hequiv; auto.
       + intros; specialize (Hvr ty); inv Hvr; auto.
-    - admit.
+    - apply EquivList_comm, methsToRep_duplicate_meths_equiv; auto.
+      + intros; specialize (Hequiv ty); inv Hequiv; auto.
+      + intros; specialize (Hvr ty); inv Hvr; auto.
   Qed.
 
 End SinModuleDup.
