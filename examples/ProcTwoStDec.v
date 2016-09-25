5730f6ec5f4ac0b2c265b4aacdfd2f804a2d2a21
Joonwon Choi <joonwonc@mit.edu>
2016-09-25 13:47:42 -0400

    A piece of ltac optimization for P2st <= Pdec

--- examples/ProcTwoStDec.v
+++ examples/ProcTwoStDec.v
@@ -118,6 +118,23 @@ Section ProcTwoStDec.
                                              getStAddr getStSrc calcStAddr getStVSrc
                                              getSrc1 execState execNextPc predictNextPc.
 
+  Ltac kregmap_red ::=
+       repeat autounfold with MethDefs in *;
+  repeat autounfold with MapDefs in *;
+  kstring_simpl; dest;
+  repeat match goal with
+         | [H: M.find _ _ = None |- _] => clear H
+         end;
+  repeat
+    (try match goal with
+         | [H: M.find ?k ?m = _ |- context[M.find ?k ?m] ] => rewrite H
+         | [H1: M.find ?k ?m = _, H2: context[M.find ?k ?m] |- _] => rewrite H1 in H2
+         | [ |- context[decKind _ _] ] =>
+           rewrite kind_eq; unfold eq_rect_r, eq_rect, eq_sym
+         | [H: context[decKind _ _] |- _] =>
+           rewrite kind_eq in H; unfold eq_rect_r, eq_rect, eq_sym in H
+         end; try subst; try findReify).
+
   Theorem p2st_refines_pdec:
     p2st <<== pdec.
   Proof. (* SKIP_PROOF_ON
