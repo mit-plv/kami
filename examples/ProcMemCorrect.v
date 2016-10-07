da43cc4cdfad86a61e21b489106d5b51eb70a91a
Joonwon Choi <joonwonc@mit.edu>
2016-10-07 00:28:35 -0400

    Proved (P4st + fifos + mcache) <= scN
    (provided mcache <= memAtomicWoQ)

--- examples/ProcMemCorrect.v
+++ examples/ProcMemCorrect.v
@@ -5,6 +5,7 @@ Require Import Kami.Syntax Kami.Semantics Kami.SemFacts Kami.RefinementFacts Kam
 Require Import Kami.Renaming Kami.Specialize Kami.Tactics Kami.Duplicate Kami.ParamDup Kami.ModuleBoundEx.
 Require Import Ex.SC Ex.ProcDec Ex.MemAtomic Ex.MemCache Ex.MemCacheSubst Ex.L1Cache.
 Require Import Ex.FifoCorrect Ex.MemCorrect Ex.ProcDecSCN Kami.ParametricSyntax.
+Require Import Ex.ProcFetchDecode Ex.ProcThreeStage Ex.ProcThreeStDec Ex.ProcFourStDec.
 
 Set Implicit Arguments.
 
@@ -31,7 +32,9 @@ Section ProcMem.
             (getSrc2: Src2T LgDataBytes RfIdx)
             (getDst: DstT LgDataBytes RfIdx)
             (exec: ExecT AddrSize LgDataBytes)
-            (getNextPc: NextPcT AddrSize LgDataBytes RfIdx).
+            (getNextPc: NextPcT AddrSize LgDataBytes RfIdx)
+            (predictNextPc: forall ty, fullType ty (SyntaxKind (Bit AddrSize)) -> (* pc *)
+                                       Expr ty (SyntaxKind (Bit AddrSize))).
 
   Variable LgNumChildren: nat.
   Definition numChildren := (wordToNat (wones LgNumChildren)).
@@ -94,7 +97,7 @@ Section ProcMem.
   Qed.
 
   Theorem pdecN_mcache_refines_scN: (pdecN ++ pmFifos ++ modFromMeta mcache)%kami <<== scN.
-  Proof. (* SKIP_PROOF_ON
+  Proof. (* SKIP_PROOF_OFF
     ketrans; [|apply pdecN_memAtomic_refines_scN with (fifoSize:= rsz FifoSize); auto].
 
     kmodular.
@@ -140,7 +143,133 @@ Section ProcMem.
           
       + apply memCache_refines_memAtomic.
         
-        END_SKIP_PROOF_ON *) admit.
+        END_SKIP_PROOF_OFF *) admit.
+  Qed.
+
+  (* Abstract d2eElt *)
+  Variable (d2eElt: Kind).
+  Variable (d2ePack:
+              forall ty,
+                Expr ty (SyntaxKind (Bit 2)) -> (* opTy *)
+                Expr ty (SyntaxKind (Bit RfIdx)) -> (* dst *)
+                Expr ty (SyntaxKind (Bit AddrSize)) -> (* addr *)
+                Expr ty (SyntaxKind (Data LgDataBytes)) -> (* val1 *)
+                Expr ty (SyntaxKind (Data LgDataBytes)) -> (* val2 *)
+                Expr ty (SyntaxKind (Data LgDataBytes)) -> (* rawInst *)
+                Expr ty (SyntaxKind (Bit AddrSize)) -> (* curPc *)
+                Expr ty (SyntaxKind (Bit AddrSize)) -> (* nextPc *)
+                Expr ty (SyntaxKind Bool) -> (* epoch *)
+                Expr ty (SyntaxKind d2eElt)).
+  Variables
+    (d2eOpType: forall ty, fullType ty (SyntaxKind d2eElt) ->
+                           Expr ty (SyntaxKind (Bit 2)))
+    (d2eDst: forall ty, fullType ty (SyntaxKind d2eElt) ->
+                        Expr ty (SyntaxKind (Bit RfIdx)))
+    (d2eAddr: forall ty, fullType ty (SyntaxKind d2eElt) ->
+                         Expr ty (SyntaxKind (Bit AddrSize)))
+    (d2eVal1 d2eVal2: forall ty, fullType ty (SyntaxKind d2eElt) ->
+                                 Expr ty (SyntaxKind (Data LgDataBytes)))
+    (d2eRawInst: forall ty, fullType ty (SyntaxKind d2eElt) ->
+                            Expr ty (SyntaxKind (Data LgDataBytes)))
+    (d2eCurPc: forall ty, fullType ty (SyntaxKind d2eElt) ->
+                          Expr ty (SyntaxKind (Bit AddrSize)))
+    (d2eNextPc: forall ty, fullType ty (SyntaxKind d2eElt) ->
+                           Expr ty (SyntaxKind (Bit AddrSize)))
+    (d2eEpoch: forall ty, fullType ty (SyntaxKind d2eElt) ->
+                          Expr ty (SyntaxKind Bool)).
+
+  Hypotheses
+    (Hd2eOpType: forall opType dst addr val1 val2 rawInst curPc nextPc epoch,
+        evalExpr (d2eOpType _ (evalExpr (d2ePack opType dst addr val1 val2 rawInst curPc nextPc epoch))) = evalExpr opType)
+    (Hd2eDst: forall opType dst addr val1 val2 rawInst curPc nextPc epoch,
+        evalExpr (d2eDst _ (evalExpr (d2ePack opType dst addr val1 val2 rawInst curPc nextPc epoch))) = evalExpr dst)
+    (Hd2eAddr: forall opType dst addr val1 val2 rawInst curPc nextPc epoch,
+        evalExpr (d2eAddr _ (evalExpr (d2ePack opType dst addr val1 val2 rawInst curPc nextPc epoch))) = evalExpr addr)
+    (Hd2eVal1: forall opType dst addr val1 val2 rawInst curPc nextPc epoch,
+        evalExpr (d2eVal1 _ (evalExpr (d2ePack opType dst addr val1 val2 rawInst curPc nextPc epoch))) = evalExpr val1)
+    (Hd2eVal2: forall opType dst addr val1 val2 rawInst curPc nextPc epoch,
+        evalExpr (d2eVal2 _ (evalExpr (d2ePack opType dst addr val1 val2 rawInst curPc nextPc epoch))) = evalExpr val2)
+    (Hd2eRawInst: forall opType dst addr val1 val2 rawInst curPc nextPc epoch,
+        evalExpr (d2eRawInst _ (evalExpr (d2ePack opType dst addr val1 val2 rawInst curPc nextPc epoch))) = evalExpr rawInst)
+    (Hd2eCurPc: forall opType dst addr val1 val2 rawInst curPc nextPc epoch,
+        evalExpr (d2eCurPc _ (evalExpr (d2ePack opType dst addr val1 val2 rawInst curPc nextPc epoch))) = evalExpr curPc)
+    (Hd2eNextPc: forall opType dst addr val1 val2 rawInst curPc nextPc epoch,
+        evalExpr (d2eNextPc _ (evalExpr (d2ePack opType dst addr val1 val2 rawInst curPc nextPc epoch))) = evalExpr nextPc)
+    (Hd2eEpoch: forall opType dst addr val1 val2 rawInst curPc nextPc epoch,
+        evalExpr (d2eEpoch _ (evalExpr (d2ePack opType dst addr val1 val2 rawInst curPc nextPc epoch))) = evalExpr epoch).
+
+  (* Abstract f2dElt *)  
+  Variable (f2dElt: Kind).
+  Variable (f2dPack:
+              forall ty,
+                Expr ty (SyntaxKind (Data LgDataBytes)) -> (* rawInst *)
+                Expr ty (SyntaxKind (Bit AddrSize)) -> (* curPc *)
+                Expr ty (SyntaxKind (Bit AddrSize)) -> (* nextPc *)
+                Expr ty (SyntaxKind Bool) -> (* epoch *)
+                Expr ty (SyntaxKind f2dElt)).
+  Variables
+    (f2dRawInst: forall ty, fullType ty (SyntaxKind f2dElt) ->
+                            Expr ty (SyntaxKind (Data LgDataBytes)))
+    (f2dCurPc: forall ty, fullType ty (SyntaxKind f2dElt) ->
+                          Expr ty (SyntaxKind (Bit AddrSize)))
+    (f2dNextPc: forall ty, fullType ty (SyntaxKind f2dElt) ->
+                           Expr ty (SyntaxKind (Bit AddrSize)))
+    (f2dEpoch: forall ty, fullType ty (SyntaxKind f2dElt) ->
+                          Expr ty (SyntaxKind Bool)).
+
+  Hypotheses (Hf2dRawInst: forall rawInst curPc nextPc epoch,
+                 evalExpr (f2dRawInst _ (evalExpr (f2dPack rawInst curPc nextPc epoch))) =
+                 evalExpr rawInst)
+             (Hf2dCurPc: forall rawInst curPc nextPc epoch,
+                 evalExpr (f2dCurPc _ (evalExpr (f2dPack rawInst curPc nextPc epoch))) =
+                 evalExpr curPc)
+             (Hf2dNextPc: forall rawInst curPc nextPc epoch,
+                 evalExpr (f2dNextPc _ (evalExpr (f2dPack rawInst curPc nextPc epoch))) =
+                 evalExpr nextPc)
+             (Hf2dEpoch: forall rawInst curPc nextPc epoch,
+                 evalExpr (f2dEpoch _ (evalExpr (f2dPack rawInst curPc nextPc epoch))) =
+                 evalExpr epoch).
+
+  (* Abstract e2wElt *)  
+  Variable (e2wElt: Kind).
+  Variable (e2wPack:
+              forall ty,
+                Expr ty (SyntaxKind d2eElt) -> (* decInst *)
+                Expr ty (SyntaxKind (Data LgDataBytes)) -> (* execVal *)
+                Expr ty (SyntaxKind e2wElt)).
+  Variables
+    (e2wDecInst: forall ty, fullType ty (SyntaxKind e2wElt) ->
+                            Expr ty (SyntaxKind d2eElt))
+    (e2wVal: forall ty, fullType ty (SyntaxKind e2wElt) ->
+                        Expr ty (SyntaxKind (Data LgDataBytes))).
+
+  Hypotheses
+    (He2wDecInst: forall decInst val,
+        evalExpr (e2wDecInst _ (evalExpr (e2wPack decInst val))) = evalExpr decInst)
+    (He2wVal: forall decInst val,
+        evalExpr (e2wVal _ (evalExpr (e2wPack decInst val))) = evalExpr val).
+
+  Definition p4stN := duplicate
+                        (p4st getOptype getLdDst getLdAddr getLdSrc calcLdAddr
+                              getStAddr getStSrc calcStAddr getStVSrc
+                              getSrc1 getSrc2 getDst exec getNextPc predictNextPc
+                              d2ePack d2eOpType d2eDst d2eAddr d2eVal1 d2eVal2
+                              d2eRawInst d2eCurPc d2eNextPc d2eEpoch
+                              f2dPack f2dRawInst f2dCurPc f2dNextPc f2dEpoch
+                              e2wPack e2wDecInst e2wVal) numChildren.
+
+  Theorem p4stN_mcache_refines_scN: (p4stN ++ pmFifos ++ modFromMeta mcache)%kami <<== scN.
+  Proof. (* SKIP_PROOF_ON
+    ketrans; [|apply pdecN_mcache_refines_scN].
+    kmodular.
+    - kdisj_edms_cms_ex numChildren.
+    - kdisj_ecms_dms_ex numChildren.
+    - kduplicated.
+      ketrans.
+      + apply p4st_refines_p3st; auto.
+      + apply p3st_refines_pdec; auto.
+    - krefl.
+      END_SKIP_PROOF_ON *) admit.
   Qed.
 
 End ProcMem.
