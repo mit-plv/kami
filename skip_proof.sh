6727a48138e5888363ec771e32cb7e7c5f023acc
Murali Vijayaraghavan <vmurali@csail.mit.edu>
2016-10-29 13:46:39 -0400

    making skip proof work on mac

--- skip_proof.sh
+++ skip_proof.sh
@@ -1,8 +1,14 @@
+PLATFORM=`uname`
+SED="sed -i"
+if [[ "$PLATFORM" == 'Darwin' ]]; then
+	SED="$SED ''"
+fi
+
 if [ "$1" = "-r" ]; then
 	for file in */*.v;
 	do
 		echo "Setting SKIP_PROOF_OFF in $file"
-		sed -i -e 's/(\* SKIP_PROOF_ON/(\* SKIP_PROOF_OFF \*)/g' \
+		$SED -e 's/(\* SKIP_PROOF_ON/(\* SKIP_PROOF_OFF \*)/g' \
 			-e 's/END_SKIP_PROOF_ON \*) apply cheat\./(\* END_SKIP_PROOF_OFF \*)/g' "$file"
 	done
 else
@@ -10,7 +16,7 @@ else
 	do
 		echo "Setting SKIP_PROOF_ON in $file"
 		#grep "SKIP_PROOF" "$file" > /dev/null &&
-		sed -i -e 's/(\* SKIP_PROOF_OFF \*)/(\* SKIP_PROOF_ON/g' \
+		$SED -e 's/(\* SKIP_PROOF_OFF \*)/(\* SKIP_PROOF_ON/g' \
 			-e 's/(\* END_SKIP_PROOF_OFF \*)/END_SKIP_PROOF_ON \*) apply cheat\./g' "$file"
 	done
 fi
