44885f39bca8d2e0612e7d74107737ba66607e09
Murali Vijayaraghavan <vmurali@csail.mit.edu>
2017-03-25 22:52:28 -0400

    more synthesis

--- compilation/PrettyPrint.hs
+++ compilation/PrettyPrint.hs
@@ -4,16 +4,21 @@ import Data.List
 ppVecLen :: Int -> String
 ppVecLen n = "[" ++ show (2 ^ n) ++ "]"
 
-{-
-ppVecArray :: Kind -> String
-ppVecArray (Vector k i) = (ppVecLen i ++ fst v, snd v)
-                          where v = ppVecArray k
-ppVecArray k = ("", k)
--}
+finToInt :: T -> Int
+finToInt (F1 _) = 0
+finToInt (FS _ x) = succ (finToInt x)
 
 vectorToList :: T0 a -> [a]
 vectorToList Nil = []
 vectorToList (Cons x _ xs) = x : vectorToList xs
+
+ilistToList :: Ilist a b -> [(a, b)]
+ilistToList Inil = []
+ilistToList (Icons a _ _ b vs) = (a,b): ilistToList vs
+
+vecToList :: Vec a -> [a]
+vecToList (Vec0 a) = [a]
+vecToList (VecNext _ xs ys) = vecToList xs ++ vecToList ys
 
 ppDecl :: String -> (String, [Int]) -> String
 ppDecl s (x, v) = x ++ ' ' : s ++ concatMap ppVecLen v
@@ -25,14 +30,22 @@ ppType (Bit i) = if i > 0
                  else ("logic", [])
 ppType (Vector k i) = (fst v, i : snd v)
                       where v = ppType k
-ppType (Struct n v) = ("struct " ++ concatMap (\x -> ppDecl (attrName x) (ppType (attrType x)) ++ "; ") (vectorToList v), [])
+ppType (Struct n v) = ("struct {" ++ concatMap (\x -> ppDecl (attrName x) (ppType (attrType x)) ++ "; ") (vectorToList v) ++ "}", [])
+
+
+replChar :: Char -> String
+replChar '.' = "$_$"
+replChar c = c : []
+
+sanitizeString :: String -> String
+sanitizeString s = concatMap replChar s
 
 
 ppDeclType :: String -> Kind -> String
 ppDeclType s k = ppDecl s (ppType k)
 
 ppPrintVar :: (String, [Int]) -> String
-ppPrintVar (s, vs) = s ++ concatMap (\v -> '$' : show v) vs
+ppPrintVar (s, vs) = sanitizeString s ++ concatMap (\v -> '$' : show v) vs
 
 ppWord :: [Bool] -> String
 ppWord [] = []
@@ -40,7 +53,9 @@ ppWord (b : bs) = (if b then '1' else '0') : ppWord bs
 
 ppConst :: ConstT -> String
 ppConst (ConstBool b) = if b then "1'b1" else "1'b0"
-ppConst (ConstBit sz w) = show sz ++ '\'' : ppWord w
+ppConst (ConstBit sz w) = if sz == 0 then "1'b0" else show sz ++ "\'b" ++ ppWord w
+ppConst (ConstVector k _ vs) = '{' : intercalate ", " (map ppConst (vecToList vs)) ++ "}"
+ppConst (ConstStruct _ _ is) = '{' : intercalate ", " (map ppConst (snd (unzip (ilistToList is)))) ++ "}"
 
 ppRtlExpr :: RtlExpr -> String
 ppRtlExpr (RtlReadReg k s) = s
@@ -51,11 +66,16 @@ ppRtlExpr (RtlUniBool Neg e) = "(~" ++ ppRtlExpr e ++ ")"
 ppRtlExpr (RtlBinBool And e1 e2) = '(' : ppRtlExpr e1 ++ " & " ++ ppRtlExpr e2 ++ ")"
 ppRtlExpr (RtlBinBool Or e1 e2) = '(' : ppRtlExpr e1 ++ " | " ++ ppRtlExpr e2 ++ ")"
 ppRtlExpr (RtlUniBit _ _ (Inv _) e) = "(~" ++ ppRtlExpr e ++ ")"
-ppRtlExpr (RtlUniBit _ _ (ConstExtract a b c) e) = ""
-ppRtlExpr (RtlUniBit _ _ (Trunc _ _) e) = ""
-ppRtlExpr (RtlUniBit _ _ (TruncLsb _ _) e) = ""
-ppRtlExpr (RtlUniBit _ _ (ZeroExtendTrunc _ _) e) = ""
-ppRtlExpr (RtlUniBit _ _ (SignExtendTrunc _ _) e) = ""
+ppRtlExpr (RtlUniBit sz retSz (ConstExtract lsb ret msb) e) = ppRtlExpr e ++ '[' : show (sz - msb - 1) ++ ':' : show lsb ++ "]"
+ppRtlExpr (RtlUniBit sz retSz (Trunc _ _) e) = ppRtlExpr e ++ '[' : show (retSz - 1) ++ ":0]"
+ppRtlExpr (RtlUniBit sz retSz (TruncLsb lsb _) e) = ppRtlExpr e ++ '[' : show (sz - 1) ++ ':' : show lsb ++ "]"
+ppRtlExpr (RtlUniBit sz retSz (ZeroExtendTrunc _ _) e) = if sz < retSz
+                                                         then "{ " ++ show (retSz - sz) ++ "'b0, " ++ ppRtlExpr e ++ "}"
+                                                         else ppRtlExpr e ++ '[' : show (retSz - 1) ++ ":0]"
+ppRtlExpr (RtlUniBit sz retSz (SignExtendTrunc _ _) e) = if sz < retSz
+                                                         then "{{" ++ show (retSz - sz) ++ '{' : ppRtlExpr e ++ "[" ++
+                                                              show sz ++ "]" ++ "}}, " ++ ppRtlExpr e ++ "}"
+                                                         else ppRtlExpr e ++ '[' : show (retSz - 1) ++ ":0]"
 ppRtlExpr (RtlBinBit _ _ _ (Add _) e1 e2) = '(' : ppRtlExpr e1 ++ " + " ++ ppRtlExpr e2 ++ ")"
 ppRtlExpr (RtlBinBit _ _ _ (Sub _) e1 e2) = '(' : ppRtlExpr e1 ++ " - " ++ ppRtlExpr e2 ++ ")"
 ppRtlExpr (RtlBinBit _ _ _ (Mul _) e1 e2) = '(' : ppRtlExpr e1 ++ " * " ++ ppRtlExpr e2 ++ ")"
@@ -66,6 +86,35 @@ ppRtlExpr (RtlBinBit _ _ _ (Sll _ _) e1 e2) = '(' : ppRtlExpr e1 ++ " << " ++ pp
 ppRtlExpr (RtlBinBit _ _ _ (Srl _ _) e1 e2) = '(' : ppRtlExpr e1 ++ " >> " ++ ppRtlExpr e2 ++ ")"
 ppRtlExpr (RtlBinBit _ _ _ (Sra _ _) e1 e2) = "($signed("  ++ ppRtlExpr e1 ++ ") >>> " ++ ppRtlExpr e2 ++ ")"
 ppRtlExpr (RtlBinBit _ _ _ (Concat _ _) e1 e2) = '{' : ppRtlExpr e1 ++ ", " ++ ppRtlExpr e2 ++ "}"
+ppRtlExpr (RtlBinBitBool _ _ (Lt _) e1 e2) =  '(' : ppRtlExpr e1 ++ "<" ++ ppRtlExpr e2 ++ "}"
+ppRtlExpr (RtlITE _ p e1 e2) = '(' : ppRtlExpr p ++ '?' : ppRtlExpr e1 ++ ':' : ppRtlExpr e2 ++ ")"
+ppRtlExpr (RtlEq _ e1 e2) = '(' : ppRtlExpr e1 ++ "==" ++ ppRtlExpr e2 ++ ")"
+ppRtlExpr (RtlReadIndex _ _ v i) = ppRtlExpr v ++ '[' : ppRtlExpr i ++ "]"
+ppRtlExpr (RtlReadField _ fields idx s) = ppRtlExpr s ++ '.' : attrName (vectorToList fields !! finToInt idx)
+ppRtlExpr (RtlBuildVector _ _ vs) = '{' : intercalate ", " (map ppRtlExpr (vecToList vs)) ++ "}"
+ppRtlExpr (RtlBuildStruct _ _ es) = '{' : intercalate ", " (map (\x -> ppRtlExpr (snd x)) (ilistToList es)) ++ "}"
+ppRtlExpr (RtlUpdateVector n _ v idx val) =
+  '{' : intercalate ", "
+  (map (\i -> ppRtlExpr idx ++ "==" ++ show n ++ "'d" ++ show i ++ " ? " ++ ppRtlExpr val ++
+              ":" ++ ppRtlExpr v ++ "[" ++ show i ++ "]")
+   [0 .. ((2^n)-1)]) ++ "}"
+
+ppRtlModule :: RtlModule -> String
+ppRtlModule (Build_RtlModule ins outs regs assigns) =
+  "module top(\n" ++
+  concatMap (\(nm, ty) -> "  input " ++ ppDeclType (ppPrintVar nm) ty ++ ",\n") ins ++ "\n" ++
+  concatMap (\(nm, ty) -> "  output " ++ ppDeclType (ppPrintVar nm) ty ++ ",\n") outs ++ "\n" ++
+  "  input CLK,\n" ++
+  "  input RESET\n" ++
+  ");\n" ++
+  concatMap (\(nm, (ty, (init, expr))) -> "  " ++ ppDeclType (sanitizeString nm) ty ++ ";\n") regs ++ "\n" ++
+  concatMap (\(nm, (ty, expr)) -> "  " ++ ppDeclType (ppPrintVar nm) ty ++ ";\n") assigns ++ "\n" ++
+  concatMap (\(nm, (ty, expr)) -> "  assign " ++ ppPrintVar nm ++ " = " ++ ppRtlExpr expr ++ ";\n") assigns ++ "\n" ++
+  "  always @(posedge CLK) begin\n" ++
+  concatMap (\(nm, (ty, (init, expr))) -> "    " ++ sanitizeString nm ++
+                                          " <= RESET? " ++ ppConst init ++ ":" ++ ppRtlExpr expr ++ ";\n") regs ++
+  "  end\n\n" ++
+  "endmodule\n"
 
 main =
-  putStrLn "Hello world"
+  putStrLn (ppRtlModule p4stRtl)
