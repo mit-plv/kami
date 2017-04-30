41bbdc11ab4e9ae50c7ca8db7211e170e66734b0
Murali Vijayaraghavan <vmurali@csail.mit.edu>
2017-04-30 07:09:13 +0530

    added parametric files

--- src/Kami.v
+++ src/Kami.v
@@ -1,8 +1,9 @@
 Require Export Bool String List.
 
-Require Export Lib.FMap Lib.Word.
+Require Export Lib.FMap Lib.Word Lib.CommonTactics.
 
 Require Export Kami.Syntax Kami.Semantics Kami.Wf Kami.Notations.
+Require Export Kami.ParametricSyntax Kami.ParametricEquiv Kami.ParametricInline Kami.ParametricInlineLtac Kami.ParametricWf Kami.PartialInlineFacts.
 Require Export Kami.RefinementFacts Kami.Decomposition.
 Require Export Kami.Inline Kami.InlineFacts.
 Require Export Kami.Tactics.
