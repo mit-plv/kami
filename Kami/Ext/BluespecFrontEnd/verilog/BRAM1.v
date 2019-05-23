c106e1435dad654b29a63da0b42f76f0f85fa3da
Joonwon Choi <joonwonc@mit.edu>
2019-05-23 15:19:29 -0400

    Added the processor verilog code that uses BRAM

---
+++ Kami/Ext/BluespecFrontEnd/verilog/BRAM1.v
@@ -0,0 +1,85 @@
+// Copyright (c) 2000-2011 Bluespec, Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// $Revision: 28325 $
+// $Date: 2012-04-25 18:22:57 +0000 (Wed, 25 Apr 2012) $
+
+`ifdef BSV_ASSIGNMENT_DELAY
+`else
+ `define BSV_ASSIGNMENT_DELAY
+`endif
+
+// Single-Ported BRAM
+module BRAM1(CLK,
+             EN,
+             WE,
+             ADDR,
+             DI,
+             DO
+             );
+
+   parameter                      PIPELINED  = 0;
+   parameter                      ADDR_WIDTH = 1;
+   parameter                      DATA_WIDTH = 1;
+   parameter                      MEMSIZE    = 1;
+
+   input                          CLK;
+   input                          EN;
+   input                          WE;
+   input [ADDR_WIDTH-1:0]         ADDR;
+   input [DATA_WIDTH-1:0]         DI;
+   output [DATA_WIDTH-1:0]        DO;
+
+   reg [DATA_WIDTH-1:0]           RAM[0:MEMSIZE-1];
+   reg [DATA_WIDTH-1:0]           DO_R;
+   reg [DATA_WIDTH-1:0]           DO_R2;
+
+`ifdef BSV_NO_INITIAL_BLOCKS
+`else
+   // synopsys translate_off
+   integer                        i;
+   initial
+   begin : init_block
+      for (i = 0; i < MEMSIZE; i = i + 1) begin
+         RAM[i] = { ((DATA_WIDTH+1)/2) { 2'b10 } };
+      end
+      DO_R  = { ((DATA_WIDTH+1)/2) { 2'b10 } };
+      DO_R2 = { ((DATA_WIDTH+1)/2) { 2'b10 } };
+   end
+   // synopsys translate_on
+`endif // !`ifdef BSV_NO_INITIAL_BLOCKS
+
+   always @(posedge CLK) begin
+      if (EN) begin
+         if (WE) begin
+            RAM[ADDR] <= `BSV_ASSIGNMENT_DELAY DI;
+            DO_R <= `BSV_ASSIGNMENT_DELAY DI;
+         end
+         else begin
+            DO_R <= `BSV_ASSIGNMENT_DELAY RAM[ADDR];
+         end
+      end
+      DO_R2 <= `BSV_ASSIGNMENT_DELAY DO_R;
+   end
+
+   // Output driver
+   assign DO = (PIPELINED) ? DO_R2 : DO_R;
+
+endmodule // BRAM1
