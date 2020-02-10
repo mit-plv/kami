cc986371ccdd7c1b149ada044d7b4e9c7f286ddf
Joonwon Choi <joonwonc@mit.edu>
2020-02-10 14:03:56 -0500

    Started working on testing framework

---
+++ riscv-test/compliance_test.h
@@ -0,0 +1,17 @@
+// RISC-V Compliance Test Header File
+
+#ifndef _COMPLIANCE_TEST_H
+#define _COMPLIANCE_TEST_H
+
+//---------------------
+// RV Compliance Macros
+//---------------------
+
+#define RV_COMPLIANCE_HALT
+#define RV_COMPLIANCE_RV32M
+#define RV_COMPLIANCE_CODE_BEGIN
+#define RV_COMPLIANCE_CODE_END
+#define RV_COMPLIANCE_DATA_BEGIN
+#define RV_COMPLIANCE_DATA_END
+
+#endif // _COMPLIANCE_TEST_H
