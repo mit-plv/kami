a25af08eeef56e8c9b3bb75148f853f1b865f6e1
Joonwon Choi <joonwonc@mit.edu>
2020-02-13 20:33:27 -0500

    Resurrect some examples

---
+++ Kami/Ex/Fifo.v
@@ -0,0 +1,107 @@
+Require Import Bool String List.
+Require Import Lib.CommonTactics Lib.ilist Lib.Word Lib.Indexer Lib.StringAsList.
+Require Import Kami.Syntax Kami.Notations Kami.Semantics.
+Require Import Kami.Wf Kami.Tactics.
+Require Import FunctionalExtensionality Eqdep Eqdep_dec.
+
+Set Implicit Arguments.
+  
+Section Fifo.
+  Variable fifoName: string.
+  Variable sz: nat.
+  Variable dType: Kind.
+
+  Local Notation "^ s" := (fifoName -- s) (at level 0).
+
+  Definition enq {ty} : forall (d: ty dType), ActionT ty Void := fun d =>
+    (Read isFull <- ^"full";
+     Assert !#isFull;
+     Read eltT <- ^"elt";
+     Read enqPT <- ^"enqP";
+     Read deqPT <- ^"deqP";
+     Write ^"elt" <- #eltT@[#enqPT <- #d];
+     Write ^"empty" <- $$false;
+     LET next_enqP <- (#enqPT + $1) :: Bit sz;
+     Write ^"full" <- (#deqPT == #next_enqP);
+     Write ^"enqP" <- #next_enqP;
+     Retv)%kami_action.
+
+  Definition deq {ty} : ActionT ty dType :=
+    (Read isEmpty <- ^"empty";
+     Assert !#isEmpty;
+     Read eltT <- ^"elt";
+     Read enqPT <- ^"enqP";
+     Read deqPT <- ^"deqP";
+     Write ^"full" <- $$false;
+     LET next_deqP <- (#deqPT + $1) :: Bit sz;
+     Write ^"empty" <- (#enqPT == #next_deqP);
+     Write ^"deqP" <- #next_deqP;
+     Ret #eltT@[#deqPT])%kami_action.
+
+  Definition firstElt {ty} : ActionT ty dType :=
+    (Read isEmpty <- ^"empty";
+     Assert !#isEmpty;
+     Read eltT : Vector dType sz <- ^"elt";
+     Read deqPT <- ^"deqP";
+     Ret #eltT@[#deqPT])%kami_action.
+  
+  Definition fifo := MODULE {
+    Register ^"elt" : Vector dType sz <- Default
+    with Register ^"enqP" : Bit sz <- Default
+    with Register ^"deqP" : Bit sz <- Default
+    with Register ^"empty" : Bool <- true
+    with Register ^"full" : Bool <- Default
+
+    with Method ^"enq"(d : dType) : Void := (enq d)
+    with Method ^"deq"() : dType := deq
+    with Method ^"firstElt"() : dType := firstElt
+  }.
+
+  Definition simpleFifo := MODULE {
+    Register ^"elt" : Vector dType sz <- Default
+    with Register ^"enqP" : Bit sz <- Default
+    with Register ^"deqP" : Bit sz <- Default
+    with Register ^"empty" : Bool <- true
+    with Register ^"full" : Bool <- Default
+
+    with Method ^"enq"(d : dType) : Void := (enq d)
+    with Method ^"deq"() : dType := deq
+  }.
+
+End Fifo.
+
+Hint Unfold fifo simpleFifo : ModuleDefs.
+Hint Unfold enq deq firstElt : MethDefs.
+
+Section Facts.
+  Variable fifoName: string.
+  Variable sz: nat.
+  Variable dType: Kind.
+
+  Hypothesis HfifoName: index 0 indexSymbol fifoName = None.
+
+  Lemma fifo_ModEquiv:
+    ModPhoasWf (fifo fifoName sz dType).
+  Proof. kequiv. Qed.
+  Hint Resolve fifo_ModEquiv.
+
+  Lemma simpleFifo_ModEquiv:
+    ModPhoasWf (simpleFifo fifoName sz dType).
+  Proof. kequiv. Qed.
+  Hint Resolve simpleFifo_ModEquiv.
+
+  Lemma fifo_ValidRegs:
+    ModRegsWf (fifo fifoName sz dType).
+  Proof. kvr. Qed.
+  Hint Resolve fifo_ValidRegs.
+
+  Lemma simpleFifo_ValidRegs:
+    ModRegsWf (simpleFifo fifoName sz dType).
+  Proof. kvr. Qed.
+  Hint Resolve simpleFifo_ValidRegs.
+
+End Facts.
+
+Hint Resolve fifo_ModEquiv simpleFifo_ModEquiv.
+Hint Resolve fifo_ValidRegs simpleFifo_ValidRegs.
+
---
+++ Kami/Ex/NativeFifo.v
@@ -0,0 +1,118 @@
+Require Import Arith.Peano_dec Bool String List.
+Require Import Lib.CommonTactics Lib.ilist Lib.Word Lib.Indexer Lib.StringAsList Lib.StringEq.
+Require Import Kami.Syntax Kami.Notations Kami.Semantics.
+Require Import Kami.Wf Kami.Tactics.
+
+Import ListNotations.
+
+Set Implicit Arguments.
+
+Section NativeFifo.
+  Variable fifoName: string.
+  
+  Variable dType: Kind.
+  Variable default: ConstT dType.
+
+  Local Notation "^ s" := (fifoName -- s) (at level 0).
+
+  Definition listEltT ty := list (ty dType).
+  Definition listEltK ty := @NativeKind (listEltT ty) nil.
+  Definition listElt ty := (^"elt" :: (@NativeConst (listEltT ty) nil nil))%struct.
+
+  Definition listIsEmpty {ty} (l: fullType ty (listEltK ty)) :=
+    match l with
+    | nil => ConstBool true
+    | _ => ConstBool false
+    end.
+  Definition listEnq {ty} (a: ty dType) (l: fullType ty (listEltK ty)) :=
+    l ++ [a].
+  Definition listDeq {ty} (l: fullType ty (listEltK ty)) :=
+    match l with
+    | nil => nil
+    | h :: t => t
+    end.
+  Definition listFirstElt {ty} (l: fullType ty (listEltK ty)): Expr ty (SyntaxKind dType) :=
+    match l with
+    | nil => ($$default)%kami_expr
+    | h :: t => (#h)%kami_expr
+    end.
+
+  (* defined methods *)
+  Definition nativeEnq {ty} : forall (d: ty dType), ActionT ty Void := fun d =>
+    (ReadN eltT : listEltK ty <- ^"elt";
+     Write ^"elt" <- (Var _ (listEltK ty) (listEnq d eltT));
+     Retv)%kami_action.
+
+  Definition nativeDeq {ty} : ActionT ty dType :=
+    (ReadN eltT : listEltK ty <- ^"elt";
+     Assert !$$(listIsEmpty eltT);
+     Write ^"elt" <- (Var _ (listEltK ty) (listDeq eltT));
+     Ret (listFirstElt eltT))%kami_action.
+
+  Definition nativeFirstElt {ty} : ActionT ty dType :=
+    (ReadN elt : listEltK ty <- ^"elt";
+     Assert !$$(listIsEmpty elt);
+     Ret (listFirstElt elt))%kami_action.
+
+  Definition nativeFifo := MODULE {
+    RegisterN ^"elt" : listEltK type <- (NativeConst nil nil)
+
+    with Method ^"enq"(d : dType) : Void := (nativeEnq d)
+    with Method ^"deq"() : dType := nativeDeq
+    with Method ^"firstElt"() : dType := nativeFirstElt
+  }.
+
+  Definition nativeSimpleFifo := MODULE {
+    RegisterN ^"elt" : listEltK type <- (NativeConst nil nil)
+
+    with Method ^"enq"(d : dType) : Void := (nativeEnq d)
+    with Method ^"deq"() : dType := nativeDeq
+  }.
+
+End NativeFifo.
+
+Hint Unfold nativeFifo nativeSimpleFifo : ModuleDefs.
+Hint Unfold listEltT listEltK listElt
+     (* listIsEmpty listEnq listDeq listFirstElt *)
+     nativeEnq nativeDeq nativeFirstElt: MethDefs.
+
+Section Facts.
+  Variable fifoName: string.
+  Variable dType: Kind.
+  Variable default: ConstT dType.
+
+  Hypothesis (Hgood: index 0 indexSymbol fifoName = None).
+
+  Lemma nativeFifo_ModEquiv:
+    ModPhoasWf (nativeFifo fifoName default).
+  Proof.
+    kequiv.
+  Qed.
+  Hint Resolve nativeFifo_ModEquiv.
+
+  Lemma nativeSimpleFifo_ModEquiv:
+    ModPhoasWf (nativeSimpleFifo fifoName default).
+  Proof.
+    kequiv.
+  Qed.
+  Hint Resolve nativeSimpleFifo_ModEquiv.
+
+  Lemma nativeFifo_ValidRegs:
+    ModRegsWf (nativeFifo fifoName default).
+  Proof.
+    kvr.
+  Qed.
+  Hint Resolve nativeFifo_ValidRegs.
+
+  Lemma nativeSimpleFifo_ValidRegs:
+    ModRegsWf (nativeSimpleFifo fifoName default).
+  Proof.
+    kvr.
+  Qed.
+  Hint Resolve nativeSimpleFifo_ValidRegs.
+
+End Facts.
+
+Hint Resolve nativeFifo_ModEquiv nativeSimpleFifo_ModEquiv.
+Hint Resolve nativeFifo_ValidRegs nativeSimpleFifo_ValidRegs.
+
